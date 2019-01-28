432d6307e38e4f9d93fa5345eb6a16d0d8e82c68
Jonathan Couldridge <jcouldridge@outlook.com>
2019-01-28 09:34:34 +0000

    added smooth's normalize

--- Decsys/ClientApp/src/App.js
+++ Decsys/ClientApp/src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Normalize, Button } from "@smooth-ui/core-sc";
 
 class App extends Component {
   render() {
     return (
-      <p>Hello, World!</p>
+      <>
+        <Normalize />
+        <Button>Hello, World!</Button>
+      </>
     );
   }
 }
