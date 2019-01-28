99fcba33476bba861a8cab4e2886c7f34e9f8e67
Jonathan Couldridge <jcouldridge@outlook.com>
2019-01-28 09:41:34 +0000

    added react-router

--- Decsys/ClientApp/src/App.js
+++ Decsys/ClientApp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Normalize, Button } from "@smooth-ui/core-sc";
 
 class App extends Component {
@@ -6,7 +7,23 @@ class App extends Component {
     return (
       <>
         <Normalize />
-        <Button>Hello, World!</Button>
+        <Router>
+          <>
+            <Button as={Link} to="/">
+              Home
+            </Button>
+            <Button as={Link} to="/1">
+              Page 1
+            </Button>
+            <Button as={Link} to="/2">
+              Page 1
+            </Button>
+
+            <Route path="/" exact render={() => <p>Hello, World!</p>} />
+            <Route path="/1" render={() => <p>Page 1</p>} />
+            <Route path="/2" render={() => <p>Page 2</p>} />
+          </>
+        </Router>
       </>
     );
   }
