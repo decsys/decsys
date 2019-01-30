4c32deb4a0aab956a7e0b2f15c2b1ed33925290b
Jonathan Couldridge <jcouldridge@outlook.com>
2019-01-30 10:55:21 +0000

    sorting and filtering refactoring

--- Decsys/ClientApp/src/components/Admin/SurveyCardList.js
+++ Decsys/ClientApp/src/components/Admin/SurveyCardList.js
@@ -4,57 +4,61 @@ import { CaretDown, CaretUp } from "styled-icons/fa-solid";
 
 import SurveyCard from "./SurveyCard";
 
-const SortButton = props => (
+const SortButton = ({ sortby, current, sorter, children }) => (
   <Button
     variant="white"
-    fontWeight={props.current === props.sortprop ? "bold" : "normal"}
-    onClick={e => props.sorter(props.sortprop, e)}
+    fontWeight={current.key === sortby ? "bold" : "normal"}
+    onClick={e => sorter(sortby, e)}
   >
-    {props.children}{" "}
-    {props[props.sortprop] ? <CaretUp size="1em" /> : <CaretDown size="1em" />}
+    {children}{" "}
+    {current[sortby] ? <CaretUp size="1em" /> : <CaretDown size="1em" />}
   </Button>
 );
+
+const SortButtonsPanel = ({ keys, current, sorter }) =>
+  keys.map((sortKey, i) => {
+    const button = (label, key) => (
+      <SortButton key={i} sortby={key} current={current} sorter={sorter}>
+        {label}
+      </SortButton>
+    );
+    return typeof sortKey === "string"
+      ? button(sortKey, sortKey.toLocaleLowerCase())
+      : button(sortKey[0], sortKey[1]);
+  });
 
 class SurveyCardList extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      surveys: [
-        { id: 1, name: "Jon Survey", runCount: 15, active: false },
-        { id: 3, name: "Abc Survey", runCount: 10, active: true },
-        { id: 2, name: "Lol Survey", runCount: 0, active: true }
-      ],
-      sort: { name: true }
+      surveys: {
+        1: { id: 1, name: "Jon Survey", runCount: 15, active: false },
+        2: { id: 3, name: "Abc Survey", runCount: 10, active: true },
+        3: { id: 2, name: "Lol Survey", runCount: 0, active: true }
+      },
+      sort: { key: "active", name: true }
     };
   }
 
-  componentDidMount() {
-    this.numberSort("active"); //initial sort
-  }
+  getPropertySorter = key => {
+    switch (key) {
+      case "name":
+        return ({ [key]: a }, { [key]: b }) =>
+          this.state.sort[key] ? a.localeCompare(b) : b.localeCompare(a);
+      default:
+        return ({ [key]: a }, { [key]: b }) =>
+          this.state.sort[key] ? a - b : b - a;
+    }
+  };
 
-  numberSort = sortProp =>
-    this.sortBy(sortProp, ({ [sortProp]: a }, { [sortProp]: b }, asc) =>
-      asc ? a - b : b - a
-    );
-
-  stringSort = sortProp =>
-    this.sortBy(sortProp, ({ [sortProp]: a }, { [sortProp]: b }, asc) =>
-      asc ? a.localeCompare(b) : b.localeCompare(a)
-    );
-
-  sortBy = (sortProp, sorter) => {
-    this.setState(prev => {
+  sortChange = key => {
+    this.setState(({ sort }) => {
       const ascending =
-        prev.sort.current === sortProp
-          ? !prev.sort[sortProp] || false
-          : prev.sort[sortProp] || false;
-      prev.surveys.sort((a, b) => sorter(a, b, ascending));
-      prev.sort.current = sortProp;
-      prev.sort[sortProp] = ascending;
-      return {
-        sort: prev.sort,
-        surveys: prev.surveys
-      };
+        sort.key === key ? !sort[key] || false : sort[key] || false;
+      sort.key = key;
+      sort[key] = ascending;
+      return { sort };
     });
   };
 
@@ -63,45 +67,33 @@ class SurveyCardList extends Component {
   render() {
     return (
       <>
-        <Box display="flex" alignItems="center" mb="1em">
+        <Row alignItems="center" mb="1em">
           <Box uiAs="span" mr=".5em" display={{ xs: "none", md: "inline" }}>
             Sort by:
           </Box>
-          <SortButton
-            {...this.state.sort}
-            sortprop="active"
-            sorter={this.numberSort}
-          >
-            Active
-          </SortButton>
-          <SortButton
-            {...this.state.sort}
-            sortprop="name"
-            sorter={this.stringSort}
-          >
-            Name
-          </SortButton>
-          <SortButton
-            {...this.state.sort}
-            sortprop="runCount"
-            sorter={this.numberSort}
-          >
-            Run Count
-          </SortButton>
+
+          <SortButtonsPanel
+            keys={["Active", "Name", ["Run Count", "runCount"]]}
+            current={this.state.sort}
+            sorter={this.sortChange}
+          />
+
           <Input
             size="sm"
             ml="auto"
             placeholder="Filter"
             onChange={this.filterChange}
           />
-        </Box>
+        </Row>
 
-        {this.state.surveys
+        {Object.keys(this.state.surveys)
+          .map(k => this.state.surveys[k])
           .filter(({ name }) =>
             this.state.filter != null
               ? new RegExp(this.state.filter, "i").test(name)
               : true
           )
+          .sort(this.getPropertySorter(this.state.sort.key))
           .map((survey, i) => (
             <Row key={i}>
               <SurveyCard {...survey} />
--- Decsys/ClientApp/src/components/Admin/SurveyList.js
+++ Decsys/ClientApp/src/components/Admin/SurveyList.js
@@ -7,13 +7,11 @@ import SurveyCardList from "./SurveyCardList";
 const SurveyList = props => (
   <>
     <Grid>
-      <Row my="3em">
+      <Row my="3em" alignItems="center" justifyContent="space-between">
         <Typography variant="h1">My Surveys</Typography>
-        <Box ml="auto" display="flex" alignItems="center">
-          <Button variant="secondary">
-            <PlusCircle size="1em" /> Create New Survey
-          </Button>
-        </Box>
+        <Button variant="secondary">
+          <PlusCircle size="1em" /> Create New Survey
+        </Button>
       </Row>
       <Row>
         <Alert width="100%" variant="info">
