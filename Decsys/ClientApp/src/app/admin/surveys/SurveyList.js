9d1fa0725fb67e8b8666e4288fe378f2cffb7163
Jonathan Couldridge <jcouldridge@outlook.com>
2019-02-01 16:00:41 +0000

    trying to figure out sorting state

--- Decsys/ClientApp/src/app/admin/surveys/SurveyList.js
+++ Decsys/ClientApp/src/app/admin/surveys/SurveyList.js
@@ -2,18 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { getSurveysSortOrder } from "./_selectors";
 import Survey from "./Survey";
+import SortButtonsPanel from "./SortButtonsPanel";
 
-const SurveyList = ({ surveys, surveysSortOrder }) => (
+let SurveyList = ({ surveys, surveysSortOrder }) => (
   <>
+    <SortButtonsPanel />
+
     {surveysSortOrder.map(
       id => !!surveys[id] && <Survey key={id} {...surveys[id]} />
     )}
   </>
 );
 
-const Surveys = connect(state => ({
+SurveyList = connect(state => ({
   surveysSortOrder: getSurveysSortOrder(state),
   surveys: state.data.surveys
 }))(SurveyList);
 
-export default Surveys;
+export default SurveyList;
