077ad12de8848fcec20551d958070ab69ae21273
Jonathan Couldridge <jcouldridge@outlook.com>
2019-02-18 09:08:15 +0000

    other dummy routes

--- Decsys/ClientApp/src/app/admin/survey/Survey.js
+++ Decsys/ClientApp/src/app/admin/survey/Survey.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom";
 import Editor from "./editor/Editor";
 import Preview from "./preview/Preview";
 import Export from "./export/Export";
+import Dashboard from "./dashboard/Dashboard";
+import Results from "./results/Dashboard";
 
 const Survey = props => (
   <Switch>
@@ -11,6 +13,10 @@ const Survey = props => (
     <Route path={`${props.match.path}/preview`} component={Preview} />
 
     <Route path={`${props.match.path}/export`} component={Export} />
+
+    <Route path={`${props.match.path}/dashboard`} component={Dashboard} />
+
+    <Route path={`${props.match.path}/results`} component={Results} />
   </Switch>
 );
 
---
+++ Decsys/ClientApp/src/app/admin/survey/dashboard/Dashboard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashboard = props => (
+  <h1>Hello I'm the current Survey Session Dashboard</h1>
+);
+
+export default Dashboard;
