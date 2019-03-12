fb646e2862a6a3a1f5b10f0d1084d3cee0db28cf
Jonathan Couldridge <jcouldridge@outlook.com>
2019-03-12 12:08:51 +0000

    fixed multiple children in AppBar

--- Decsys/ClientApp/src/app/components/AppBar/AppBar.js
+++ Decsys/ClientApp/src/app/components/AppBar/AppBar.js
@@ -4,13 +4,20 @@ import PropTypes from "prop-types";
 import FlexBox from "../ui/FlexBox";
 import Container from "../ui/Container";
 import AppBarBrand from "./AppBarBrand";
+import { Grid } from "styled-css-grid";
 
 const AppBar = ({ theme, brand, children }) => (
   <FlexBox backgroundColor={theme.gray900} alignItems="center">
     <Container>
       <FlexBox alignItems="center" justifyContent="space-between">
         <AppBarBrand>{brand}</AppBarBrand>
-        {children}
+        <Grid
+          columns={Array(children.length)
+            .fill("auto")
+            .join(" ")}
+        >
+          {children}
+        </Grid>
       </FlexBox>
     </Container>
   </FlexBox>
