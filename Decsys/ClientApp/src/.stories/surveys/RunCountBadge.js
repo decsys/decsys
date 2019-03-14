7a798c86df4f26650dbe474f16de6cb5dc6523f4
Jonathan Couldridge <jcouldridge@outlook.com>
2019-03-14 11:20:33 +0000

    some tidy-up

--- Decsys/ClientApp/src/.stories/surveys/RunCountBadge.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import RunCountBadge from "../../app/admin/surveys/RunCountBadge";
-
-storiesOf("RunCountBadge", module)
-  .add("Default", () => <RunCountBadge />)
-  .add("1 digit", () => <RunCountBadge count={5} />)
-  .add("4 digits", () => <RunCountBadge count={5000} />);
