cf10c247245c2fdc937b16a605e4b8cd8d3d026c
Jonathan Couldridge <beforan86@gmail.com>
2019-03-19 20:49:13 +0000

    Release prep (#7)
    * https redirection should now be configurably optional
    
    * getting components folder included in publish
    
    * added filler empty state component for created surveys
    
    * initial travis file, changes to support win-x64 scd
    
    * modifying msbuild project to not run webpack for us on publish

--- Decsys/ClientApp/src/app/App.js
+++ Decsys/ClientApp/src/app/App.js
@@ -5,6 +5,8 @@ import { Route, Redirect, Switch, withRouter } from "react-router-dom";
 import SurveysScreen from "./screens/admin/SurveysScreen";
 import { Container, EmptyState, FlexBox } from "./components/ui";
 import { fetchSurveys } from "./state/ducks/surveys";
+import { push } from "connected-react-router";
+import { CheckCircle } from "styled-icons/fa-solid";
 
 const PureApp = ({ dispatch, listLoaded }) => {
   return (
@@ -22,10 +24,30 @@ const PureApp = ({ dispatch, listLoaded }) => {
 
         <Route
           path="/admin"
+          exact
           render={() => {
             dispatch(fetchSurveys());
             return <SurveysScreen />;
           }}
+        />
+
+        <Route
+          path="/admin/survey/:id"
+          render={() => (
+            // TODO: temporary content before editor is ready
+            <Container>
+              <FlexBox mt={5}>
+                <EmptyState
+                  splash={<CheckCircle />}
+                  message="Your Survey was created! The Editor is not ready yet."
+                  callToAction={{
+                    label: "Back to Survey List",
+                    onClick: () => dispatch(push("/admin"))
+                  }}
+                />
+              </FlexBox>
+            </Container>
+          )}
         />
 
         <Route
