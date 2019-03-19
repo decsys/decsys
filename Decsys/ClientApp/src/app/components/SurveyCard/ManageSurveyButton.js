58ca9bfcb83e56cf9c5a2b2066c4e040205b6ed2
Jonathan Couldridge <jcouldridge@outlook.com>
2019-03-19 10:12:00 +0000

    ManageSurveyButton uses hooks now

--- Decsys/ClientApp/src/app/components/SurveyCard/ManageSurveyButton.js
+++ Decsys/ClientApp/src/app/components/SurveyCard/ManageSurveyButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { MenuItem, MenuRouterLink, DropdownMenuButton } from "../ui";
@@ -6,56 +6,50 @@ import DeleteSurveyModal from "./DeleteSurveyModal";
 import { EllipsisV } from "styled-icons/fa-solid";
 import { deleteSurvey } from "../../state/ducks/surveys";
 
-class PureManageSurveyButton extends Component {
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    editable: PropTypes.bool,
-    name: PropTypes.string.isRequired,
-    onDuplicateClick: PropTypes.func.isRequired,
-    onDeleteClick: PropTypes.func.isRequired
-  };
+const PureManageSurveyButton = ({
+  name,
+  editable,
+  id,
+  onDuplicateClick,
+  onDeleteClick
+}) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const toggleDeleteModal = () => setShowDeleteModal(!showDeleteModal);
 
-  static defaultProps = {
-    editable: false
-  };
+  return (
+    <>
+      <DropdownMenuButton
+        title="More survey actions..."
+        display="block"
+        variant="secondary"
+        button={<EllipsisV size="1em" />}
+        caret={false}
+      >
+        {editable && <MenuRouterLink to={`survey/${id}`}>Edit</MenuRouterLink>}
+        <MenuRouterLink to={`survey/${id}/preview`}>Preview</MenuRouterLink>
+        <MenuRouterLink to={`survey/${id}/export`}>Export</MenuRouterLink>
+        <MenuItem onClick={onDuplicateClick}>Duplicate</MenuItem>
+        <MenuItem onClick={toggleDeleteModal}>Delete</MenuItem>
+      </DropdownMenuButton>
+      <DeleteSurveyModal
+        surveyName={name}
+        deleteSurvey={() => onDeleteClick(id)}
+        closeModal={toggleDeleteModal}
+        modalOpened={showDeleteModal}
+      />
+    </>
+  );
+};
 
-  constructor(props) {
-    super(props);
-    this.state = { showDeleteModal: false };
-  }
+PureManageSurveyButton.propTypes = {
+  id: PropTypes.number.isRequired,
+  editable: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  onDuplicateClick: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired
+};
 
-  toggleDeleteModal = () =>
-    this.setState(prev => ({ showDeleteModal: !prev.showDeleteModal }));
-
-  render() {
-    const { name, editable, id, onDuplicateClick, onDeleteClick } = this.props;
-    return (
-      <>
-        <DropdownMenuButton
-          title="More survey actions..."
-          display="block"
-          variant="secondary"
-          button={<EllipsisV size="1em" />}
-          caret={false}
-        >
-          {editable && (
-            <MenuRouterLink to={`survey/${id}`}>Edit</MenuRouterLink>
-          )}
-          <MenuRouterLink to={`survey/${id}/preview`}>Preview</MenuRouterLink>
-          <MenuRouterLink to={`survey/${id}/export`}>Export</MenuRouterLink>
-          <MenuItem onClick={onDuplicateClick}>Duplicate</MenuItem>
-          <MenuItem onClick={this.toggleDeleteModal}>Delete</MenuItem>
-        </DropdownMenuButton>
-        <DeleteSurveyModal
-          surveyName={name}
-          deleteSurvey={() => onDeleteClick(id)}
-          closeModal={this.toggleDeleteModal}
-          modalOpened={this.state.showDeleteModal}
-        />
-      </>
-    );
-  }
-}
+PureManageSurveyButton.defaultProps = { editable: false };
 
 const ManageSurveyButton = connect(
   null,
--- Decsys/ClientApp/src/app/components/ui/DropdownMenuButton.js
+++ Decsys/ClientApp/src/app/components/ui/DropdownMenuButton.js
@@ -1,4 +1,4 @@
-import React, { Component, cloneElement, Children } from "react";
+import React, { useState, cloneElement, Children } from "react";
 import onClickOutside from "react-onclickoutside";
 import DropdownButton from "./DropdownButton";
 import DropdownMenu from "./Menu";
@@ -8,65 +8,46 @@ import DropdownMenu from "./Menu";
  *
  * All non specified props are passed onto the underlying `Button`.
  */
-class DropdownMenuButton extends Component {
-  constructor() {
-    super();
-    this.state = { open: false }; // literally we only store menu open state?
-  }
+const DropdownMenuButton = ({
+  onClick: _, //we don't want to allow an override of this
+  children,
+  // used by onClickOutside HOC
+  enableOnClickOutside,
+  disableOnClickOutside,
+  stopPropagation,
+  preventDefault,
+  outsideClickIgnoreClass,
+  eventTypes,
 
-  handleClickOutside = () => {
-    this.setState({
-      open: false
-    });
-  };
+  ...rest
+}) => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => setOpen(!open);
+  DropdownMenuButton.handleClickOutside = () => setOpen(false);
+  return (
+    <div>
+      <DropdownButton onClick={toggleMenu} {...rest} />
+      {open && (
+        <DropdownMenu>
+          {/** Modify all menu items onClick to close the mneu before taking their action */
+          Children.map(
+            children,
+            child =>
+              !!child &&
+              cloneElement(child, {
+                onClick: e => {
+                  toggleMenu();
+                  if (typeof child.props.onClick === "function")
+                    child.props.onClick(e);
+                }
+              })
+          )}
+        </DropdownMenu>
+      )}
+    </div>
+  );
+};
 
-  toggleMenu = () =>
-    this.setState(prev => ({
-      open: !prev.open
-    }));
-
-  /** Modify all menu items onClick to close the mneu before taking their action */
-  renderChildren() {
-    return Children.map(
-      this.props.children,
-      child =>
-        !!child &&
-        cloneElement(child, {
-          onClick: e => {
-            this.toggleMenu();
-            if (typeof child.props.onClick === "function")
-              child.props.onClick(e);
-          }
-        })
-    );
-  }
-
-  render() {
-    // remove props our top level component cares about
-    // or stuff we mustn't pass down to the DOM
-    const {
-      onClick: _, //we don't want to allow an override of this
-
-      // used by onClickOutside HOC
-      enableOnClickOutside,
-      disableOnClickOutside,
-      stopPropagation,
-      preventDefault,
-      outsideClickIgnoreClass,
-      eventTypes,
-
-      ...rest
-    } = this.props;
-
-    return (
-      <div>
-        <DropdownButton onClick={this.toggleMenu} {...rest} />
-        {this.state.open && (
-          <DropdownMenu>{this.renderChildren()}</DropdownMenu>
-        )}
-      </div>
-    );
-  }
-}
-
-export default onClickOutside(DropdownMenuButton);
+export default onClickOutside(DropdownMenuButton, {
+  handleClickOutside: () => DropdownMenuButton.handleClickOutside
+});
