6f090db877e5f4f984ee717e9aa9c4abe5921289
Jonathan Couldridge <jcouldridge@outlook.com>
2019-03-19 11:56:54 +0000

    create works properly now

--- Decsys/ClientApp/src/app/App.js
+++ Decsys/ClientApp/src/app/App.js
@@ -1,40 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import AppBar from "./components/AppBar";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Redirect, Switch, withRouter } from "react-router-dom";
 import SurveysScreen from "./screens/admin/SurveysScreen";
 import { Container, EmptyState, FlexBox } from "./components/ui";
 import { fetchSurveys } from "./state/ducks/surveys";
-// import AppBar from "./app/AppBar";
-// import Admin from "./app/admin/Admin";
-// import Survey from "./app/survey/Survey";
-
-// const IndexRouter = props => {
-//   // TODO: Add first time check for creating an admin account?
-
-//   // TODO: One day there will be a dashboard at `/`
-
-//   return window.location.hostname === "localhost" ? (
-//     <Redirect to="/admin" />
-//   ) : (
-//     <Survey />
-//   );
-// };
-
-/*<Route exact path="/home" render={() => (
-  isLoggedIn() ? (
-    <Redirect to="/front"/>
-  ) : (
-    <Home />
-  )
-)}/>*/
 
 const PureApp = ({ dispatch, listLoaded }) => {
   return (
     <>
       <AppBar brand="DECSYS" />
       <Switch>
-        <Route path="/" exact render={() => <Redirect to="/admin" />} />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            // TODO: conditional logic for admin
+            <Redirect to="/admin" />
+          )}
+        />
 
         <Route
           path="/admin"
@@ -59,8 +43,8 @@ const PureApp = ({ dispatch, listLoaded }) => {
   );
 };
 
-const App = connect(state => ({ listLoaded: state.surveys.listLoaded }))(
-  PureApp
+const App = withRouter(
+  connect(state => ({ listLoaded: state.surveys.listLoaded }))(PureApp)
 );
 
 export default App;
