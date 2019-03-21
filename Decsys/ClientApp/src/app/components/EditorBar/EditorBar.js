f9c6b54f3e775fddeddec6d6b699e9bcf0d1c928
Jonathan Couldridge <jcouldridge@outlook.com>
2019-03-21 14:42:52 +0000

    delete button now trigger a confirmation modal

--- Decsys/ClientApp/src/app/components/EditorBar/EditorBar.js
+++ Decsys/ClientApp/src/app/components/EditorBar/EditorBar.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Eye, Copy, FileExport, Trash } from "styled-icons/fa-solid";
 import { Grid } from "styled-css-grid";
 import EditorBarButton from "./Button";
 import NameInput from "./NameInput";
+import DeleteSurveyModal from "../SurveyCard/DeleteSurveyModal";
 
 const EditorBar = ({
   name,
@@ -13,31 +14,43 @@ const EditorBar = ({
   onDuplicateClick,
   onExportClick,
   onDeleteClick
-}) => (
-  <Grid columnGap="0px" columns="1fr auto auto auto auto">
-    <NameInput
-      name={name}
-      {...nameUpdateState}
-      onChange={onNameChange}
-      disabled={disabled}
-    />
-    <EditorBarButton onClick={onPreviewClick} disabled={disabled}>
-      <Eye size="1em" /> Preview
-    </EditorBarButton>
-    <EditorBarButton onClick={onDuplicateClick} disabled={disabled}>
-      <Copy size="1em" /> Duplicate
-    </EditorBarButton>
-    <EditorBarButton onClick={onExportClick} disabled={disabled}>
-      <FileExport size="1em" /> Export
-    </EditorBarButton>
-    <EditorBarButton
-      variant="danger"
-      onClick={onDeleteClick}
-      disabled={disabled}
-    >
-      <Trash size="1em" /> Delete
-    </EditorBarButton>
-  </Grid>
-);
+}) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const toggleDeleteModal = () => setShowDeleteModal(!showDeleteModal);
+  return (
+    <>
+      <Grid columnGap="0px" columns="1fr auto auto auto auto">
+        <NameInput
+          name={name}
+          {...nameUpdateState}
+          onChange={onNameChange}
+          disabled={disabled}
+        />
+        <EditorBarButton onClick={onPreviewClick} disabled={disabled}>
+          <Eye size="1em" /> Preview
+        </EditorBarButton>
+        <EditorBarButton onClick={onDuplicateClick} disabled={disabled}>
+          <Copy size="1em" /> Duplicate
+        </EditorBarButton>
+        <EditorBarButton onClick={onExportClick} disabled={disabled}>
+          <FileExport size="1em" /> Export
+        </EditorBarButton>
+        <EditorBarButton
+          variant="danger"
+          onClick={() => setShowDeleteModal(true)}
+          disabled={disabled}
+        >
+          <Trash size="1em" /> Delete
+        </EditorBarButton>
+      </Grid>
+      <DeleteSurveyModal
+        surveyName={name}
+        deleteSurvey={onDeleteClick}
+        closeModal={toggleDeleteModal}
+        modalOpened={showDeleteModal}
+      />
+    </>
+  );
+};
 
 export default EditorBar;
