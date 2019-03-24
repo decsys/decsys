83105334d372ec11ef58a7fa3b0c09cbcc9c9ca7
Jon Couldridge <jcouldridge@outlook.com>
2019-03-24 11:10:57 +0000

    image page item

---
+++ Decsys/ClientApp/src/app/components/page-items/Image.js
@@ -0,0 +1,19 @@
+import React from "react";
+import paramTypes, { setParams } from "../../../param-types";
+
+const PageImage = ({ id }) =>
+  // TODO: this probably shouldn't be hard coded,
+  // but it also doesn't need implementation exposing elsewhere
+  // this is clean, but we should probably move the path to a config or something
+
+  // TODO: in future we could support remote urls too
+  // but for now just local uploaded images
+
+  // atm we do nothing if the param isn't set
+  id && <img src={`/surveys/images/${id}`} />;
+
+setParams(PageParagraph, {
+  id: paramTypes.string("Image Component ID")
+});
+
+export default PageParagraph;
