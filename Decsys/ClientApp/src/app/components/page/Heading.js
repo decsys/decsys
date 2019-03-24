eb69b1ff3ade058c969ce6a8dec5c991b0c2efba
Jon Couldridge <jcouldridge@outlook.com>
2019-03-24 10:23:24 +0000

    added Heading, initial param-types modules

--- Decsys/ClientApp/src/app/components/page/Heading.js
+++ Decsys/ClientApp/src/app/components/page/Heading.js
@@ -1,1 +1,33 @@
 import React from "react";
+import { Typography } from "@smooth-ui/core-sc";
+import paramTypes, { setParams } from "../../../param-types";
+
+const PageHeading = ({ text, xMargin, ...p }) => (
+  <Typography
+    as="div"
+    {...p}
+    mr={p.textAlign === "right" ? `${xMargin}%` : 0}
+    ml={p.textAlign === "left" ? `${xMargin}%` : 0}
+  >
+    {text}
+  </Typography>
+);
+
+setParams(PageHeading, {
+  text: paramTypes.string("Text"),
+  variant: paramTypes.oneOf(
+    "Heading Style",
+    ["h1", "h2", "h3", "h4", "h5"],
+    "h2"
+  ),
+  color: paramTypes.string("Color", "black"),
+  textAlign: paramTypes.oneOf(
+    "Alignment",
+    ["left", "center", "right"],
+    "center"
+  ),
+  xMargin: paramTypes.number("Horizontal Margin", 5),
+  fontFamily: paramTypes.stringUndefined("Font Family")
+});
+
+export default PageHeading;
---
+++ Decsys/ClientApp/src/param-types/index.js
@@ -0,0 +1,16 @@
+// This is like our platform version of Prop Types,
+// because Prop Types is very closured,
+// so we can't query information about the types after the fact
+// We can use the declarations here to inform PropTypes though,
+// to avoid repetition, which is what setParams does
+import * as builders from "./builders";
+import setParams from "./setParams";
+
+// named * exports
+export * from "./builders";
+
+// other named exports
+export { setParams };
+
+// default export
+export default { setParams, ...builders };
