08a01e9a77a432da37119fd754767d6d1ee3c539
Jonathan Couldridge <jcouldridge@outlook.com>
2019-04-04 09:17:45 +0100

    fixed next button

--- Decsys/ClientApp/src/app/screens/admin/PreviewScreen.js
+++ Decsys/ClientApp/src/app/screens/admin/PreviewScreen.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { SurveyPage } from "../../components/SurveyPage";
+import SurveyPage from "../../components/SurveyPage";
 import { LoadingIndicator } from "../../components/ui";
+import AppBar, { AppBarLink } from "../../components/AppBar";
 
-const PurePreviewScreen = ({ id, survey, surveyLoaded, history }) => {
+const PurePreviewScreen = ({
+  survey: { id, pages },
+  surveyLoaded,
+  history
+}) => {
   const [page, setPage] = useState(0);
   const [lastPage, setLastPage] = useState(false);
 
-  useEffect(() => setLastPage(page === survey.pages.length - 1), [page]);
+  useEffect(() => setLastPage(page === pages.length - 1), [page]);
 
   const handleClick = () => {
     if (lastPage) history.push(`/admin/survey/${id}`);
@@ -18,9 +23,15 @@ const PurePreviewScreen = ({ id, survey, surveyLoaded, history }) => {
   return surveyLoaded ? (
     <SurveyPage
       id={id}
-      page={survey.pages[page]}
-      preview
-      onClick={handleClick}
+      page={pages[page]}
+      appBar={
+        <AppBar brand="DECSYS - Preview" brandLink="#">
+          <AppBarLink to={`/admin/survey/${id}`}>
+            Back to Survey Editor
+          </AppBarLink>
+        </AppBar>
+      }
+      onNextPage={handleClick}
       logEvent={() => {}}
       lastPage={lastPage}
     />
--- Decsys/ClientApp/src/app/screens/survey/SurveyScreen.js
+++ Decsys/ClientApp/src/app/screens/survey/SurveyScreen.js
@@ -1,120 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Cell } from "styled-css-grid";
+import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import SurveyPage from "../../components/SurveyPage";
+import { LoadingIndicator } from "../../components/ui";
 import AppBar from "../../components/AppBar";
-import { FlexBox, Container } from "../../components/ui";
-import { Button } from "@smooth-ui/core-sc";
-import { ChevronRight } from "styled-icons/fa-solid";
-import ComponentRender from "../../components/ComponentRender";
-import { getComponent } from "../../utils/component-utils";
-import Link from "../../components/AppBar/Link";
-import { COMPONENT_RESULTS, PAGE_LOAD } from "../../utils/event-types";
 
-// TODO: Prop Types!
+const PureSurveyScreen = ({ id, survey, surveyLoaded, history, logEvent }) => {
+  const [page, setPage] = useState(0);
+  const [lastPage, setLastPage] = useState(false);
 
-const PureSurveyScreen = ({
-  id,
-  page,
-  preview,
-  onNextPage,
-  lastPage,
-  logEvent
-}) => {
-  const [nextEnabled, setNextEnabled] = useState(true);
+  useEffect(() => setLastPage(page === survey.pages.length - 1), [page]);
 
-  useEffect(() => {
-    logEvent(page.id, PAGE_LOAD, {});
-  }, []);
+  const handleClick = () => {
+    if (lastPage) history.push(`/survey/complete`);
+    setPage(page + 1);
+  };
 
-  return (
-    <Grid
-      columns="1fr"
-      rows="54px minmax(20px, 1fr) 80px"
-      style={{ height: "100vh" }}
-    >
-      <Cell>
-        {preview ? (
-          <AppBar brand="DECSYS - Preview" brandLink="#">
-            <Link href={`/admin/survey/${id}`}>Back to Survey Editor</Link>
-          </AppBar>
-        ) : (
-          <AppBar brand="DECSYS" />
-        )}
-      </Cell>
-      <Cell style={{ overflow: "auto" }}>
-        <Container>
-          <FlexBox p={1} flexDirection="column">
-            {page.components.map(x => (
-              <ComponentRender
-                key={x.id}
-                component={getComponent(x.type)}
-                actions={{
-                  setNextEnabled,
-                  logEvent: (type, payload) => logEvent(x.id, type, payload),
-                  logResults: payload =>
-                    logEvent(x.id, COMPONENT_RESULTS, payload)
-                }}
-                params={
-                  x.type === "image"
-                    ? {
-                        ...x.params,
-                        surveyId: id,
-                        id: x.id
-                      }
-                    : x.params
-                }
-              />
-            ))}
-          </FlexBox>
-        </Container>
-      </Cell>
-      <Cell>
-        <Container>
-          <FlexBox p={2} justifyContent="flex-end">
-            <Button size="lg" disabled={!nextEnabled} onClick={onNextPage}>
-              {lastPage ? (
-                <>Finish</>
-              ) : (
-                <>
-                  Next <ChevronRight size="1em" />
-                </>
-              )}
-            </Button>
-          </FlexBox>
-        </Container>
-      </Cell>
-    </Grid>
+  return surveyLoaded ? (
+    <SurveyPage
+      id={id}
+      page={survey.pages[page]}
+      appBar={<AppBar brand="DECSYS" brandLink="#" />}
+      onClick={handleClick}
+      logEvent={logEvent}
+      lastPage={lastPage}
+    />
+  ) : (
+    <LoadingIndicator />
   );
 };
 
-const SurveyScreen = connect(
-  ({ user, survey: { id, pages, currentPage, instanceId } }) => ({
-    id,
-    page: pages[currentPage],
-    lastPage: currentPage === pages.length - 1,
-    instanceId,
-    userId: user.id
-  }),
-  dispatch => ({
-    onNextPage: () => dispatch({ type: "NEXT_CLICK" }),
-    logEvent: (instanceId, participantId, source, type, payload) =>
-      dispatch({ type: "LOG_EVENT" })
-  }),
-  (stateProps, dispatchProps, ownProps) => ({
-    ...ownProps,
-    ...stateProps,
-    ...dispatchProps,
-    logEvent: (source, type, payload) =>
-      dispatchProps.logEvent(
-        stateProps.instanceId,
-        stateProps.userId,
-        source,
-        type,
-        payload
-      )
-  })
-)(PureSurveyScreen);
+const SurveyScreen = withRouter(
+  connect(({ user, survey: { instanceId, survey, surveyLoaded } }) => ({
+    survey,
+    surveyLoaded
+  }))(PureSurveyScreen)
+);
 
 export { PureSurveyScreen };
 
 export default SurveyScreen;
+
+// const SurveyScreen = connect(
+//     ({ user, survey: { id, pages, currentPage, instanceId } }) => ({
+//       id,
+//       page: pages[currentPage],
+//       lastPage: currentPage === pages.length - 1,
+//       instanceId,
+//       userId: user.id
+//     }),
+//     dispatch => ({
+//       onNextPage: () => dispatch({ type: "NEXT_CLICK" }),
+//       logEvent: (instanceId, participantId, source, type, payload) =>
+//         dispatch({ type: "LOG_EVENT" })
+//     }),
+//     (stateProps, dispatchProps, ownProps) => ({
+//       ...ownProps,
+//       ...stateProps,
+//       ...dispatchProps,
+//       logEvent: (source, type, payload) =>
+//         dispatchProps.logEvent(
+//           stateProps.instanceId,
+//           stateProps.userId,
+//           source,
+//           type,
+//           payload
+//         )
+//     })
+//   )(PureSurveyScreen);
