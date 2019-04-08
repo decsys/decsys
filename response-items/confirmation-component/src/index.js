30de589caa4ee8d0e4bbb2b8c0c1058b25ba449c
Jonathan Couldridge <beforan86@gmail.com>
2019-04-08 23:07:57 +0100

    Events (#3)
    * event logging and next button control working in storybook
    
    * actions aren't wrapped by the platform

--- response-items/confirmation-component/src/index.js
+++ response-items/confirmation-component/src/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { FormCheck, Checkbox, FormCheckLabel } from "@smooth-ui/core-sc";
 import paramTypes, { setParams } from "@decsys/param-types/";
 import { Check } from "styled-icons/fa-solid/Check";
 
-const Confirm = ({ label, initialChecked }) => {
+const Confirm = ({ label, initialChecked, setNextEnabled, logResults }) => {
+  useEffect(() => setNextEnabled(false), []);
+
+  const [checked, setChecked] = useState(initialChecked);
+
   const id = new Date().getTime();
+
+  const handleChange = e => {
+    logResults(e.target.checked);
+    setChecked(e.target.checked);
+    setNextEnabled(e.target.checked);
+  };
+
   return (
     <FormCheck mr={2}>
-      <Checkbox control size="lg" id={id} checked={initialChecked} />
+      <Checkbox
+        control
+        size="lg"
+        id={id}
+        checked={checked}
+        onChange={handleChange}
+      />
       <FormCheckLabel htmlFor={id}>{label}</FormCheckLabel>
     </FormCheck>
   );
--- response-items/confirmation-component/src/index.stories.js
+++ response-items/confirmation-component/src/index.stories.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { text, boolean } from "@storybook/addon-knobs";
+import { text } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 import Confirm from "./index";
 
-storiesOf("Confirm", module).add("Default", () => (
-  <Confirm
-    label={text(Confirm.params.label.label, Confirm.params.label.defaultValue)}
-    initialChecked={boolean("Initially Checked", false)}
-  />
-));
+const actions = {
+  setNextEnabled: action("Next Button toggled"),
+  logEvent: action("Custom Event logged"),
+  logResults: action("Component Results logged")
+};
+
+storiesOf("Confirm", module)
+  .add("Default", () => (
+    <Confirm
+      label={text(
+        Confirm.params.label.label,
+        Confirm.params.label.defaultValue
+      )}
+      initialChecked={false}
+      {...actions}
+    />
+  ))
+  .add("Initially Checked", () => (
+    <Confirm
+      label={text(
+        Confirm.params.label.label,
+        Confirm.params.label.defaultValue
+      )}
+      initialChecked={true}
+      {...actions}
+    />
+  ));
