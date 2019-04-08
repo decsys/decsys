e1b7bbd0b2773976ae7fa4bd0cd71a2fef5cab41
Jonathan Couldridge <jcouldridge@outlook.com>
2019-04-08 14:36:27 +0100

    added 401 route, localadmin auth checks work

--- Decsys/ClientApp/src/app/routes.js
+++ Decsys/ClientApp/src/app/routes.js
@@ -1,44 +1,57 @@
 import React from "react";
-import { mount, route, redirect, withData } from "navi";
+import { mount, route, redirect, map, withData, withContext } from "navi";
 import * as api from "./api";
 import SurveysScreen from "./screens/admin/SurveysScreen";
 import EditorScreen from "./screens/admin/EditorScreen";
 import PreviewScreen from "./screens/admin/PreviewScreen";
+import ErrorScreen from "./screens/ErrorScreen";
 
 const routes = mount({
   "/": redirect("/admin"),
 
+  "/401": route({ view: <ErrorScreen message="401: Not Authorized" /> }),
+
   // Admin
-  "/admin": mount({
-    "/": route(async () => {
-      const { data: surveys } = await api.listSurveys();
+  "/admin": withContext(
+    () => {
+      return { localAdmin: window.location.hostname === "localhost" };
+    },
+    map(
+      (_, context) =>
+        context.localAdmin
+          ? mount({
+              "/": route(async () => {
+                const { data: surveys } = await api.listSurveys();
 
-      return {
-        view: <SurveysScreen surveys={surveys} />
-      };
-    }),
+                return {
+                  view: <SurveysScreen surveys={surveys} />
+                };
+              }),
 
-    // Survey Editor
-    "/survey": mount({
-      "/:id": withData(
-        async ({ params }) => {
-          const { data: survey } = await api.getSurvey(params.id);
-          return { survey };
-        },
-        mount({
-          "/": route(async () => ({
-            view: <EditorScreen />
-          })),
-          "/preview": route(({ params }, { test }) => ({
-            view: <PreviewScreen />
-          }))
-        })
-      ),
-      "/:id/dashboard": route(({ params }) => ({
-        view: <div>Dashboard for {params.id}</div>
-      }))
-    })
-  })
+              // Survey Editor
+              "/survey": mount({
+                "/:id": withData(
+                  async ({ params }) => {
+                    const { data: survey } = await api.getSurvey(params.id);
+                    return { survey };
+                  },
+                  mount({
+                    "/": route(async () => ({
+                      view: <EditorScreen />
+                    })),
+                    "/preview": route(() => ({
+                      view: <PreviewScreen />
+                    }))
+                  })
+                ),
+                "/:id/dashboard": route(({ params }) => ({
+                  view: <div>Dashboard for {params.id}</div>
+                }))
+              })
+            })
+          : redirect("/401") // TODO: 401 route?
+    )
+  )
 });
 
 export default routes;
