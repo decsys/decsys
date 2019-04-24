11abb51441ceb7713c0dd10ba1e347a89e1e7ab6
Jon Couldridge <jcouldridge@outlook.com>
2019-04-24 17:20:15 +0100

    Survey Configure Service method

---
+++ Decsys/Models/ConfigureSurveyModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Decsys.Models
+{
+    public class ConfigureSurveyModel
+    {
+        public bool OneTimeParticipants { get; set; }
+
+        public IEnumerable<string> ValidIdentifiers { get; set; } = new List<string>();
+    }
+}
