876359d86c7a183d5a99d30fa0625e2e2dd49d35
Jonathan Couldridge <jcouldridge@outlook.com>
2019-05-03 14:27:20 +0100

    0.3.1 fixes for buildPropTypes

--- app/param-types/buildPropTypes.js
+++ app/param-types/buildPropTypes.js
@@ -14,16 +14,16 @@ const buildPropTypes = (params, propTypes, defaultProps) => {
     defaultProps: defaultProps || {}
   };
   for (const param in params) {
-    switch (param.type) {
+    switch (params[param].type) {
       case types.string:
       case types.number:
       case types.bool:
-        x.propTypes[param] = PropTypes[param.type];
+        x.propTypes[param] = PropTypes[params[param].type];
         break;
       case types.oneOf:
         x.propTypes[param] = PropTypes.oneOf(param.oneOf);
     }
-    x.defaultProps[param] = param.defaultValue;
+    x.defaultProps[param] = params[param].defaultValue;
   }
   return x;
 };
