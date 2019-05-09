d077f80849ad14695cd7db110c443ab91b94b3fd
Jon Couldridge <jcouldridge@outlook.com>
2019-05-09 09:31:22 +0100

    random page toggle replaced with a toggle button

---
+++ Decsys/ClientApp/src/app/components/ui/ToggleButton.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "@smooth-ui/core-sc";
+
+const calculateToggleStyle = (on, variant) => {
+  if (on)
+    // use a standard full button
+    return {
+      variant,
+      border: 1,
+      borderColor: variant
+    };
+  // use an outlined button in the variant colors
+  else
+    return {
+      variant: "light",
+      color: variant,
+      border: 1,
+      borderColor: variant,
+      backgroundColor: "lightest"
+    };
+};
+
+const ToggleButton = ({
+  variant = "primary",
+  checked,
+  children,
+  onClick,
+  ...p
+}) => {
+  const [on, setOn] = useState(checked);
+  const [toggleStyle, setToggleStyle] = useState(
+    calculateToggleStyle(checked, variant)
+  );
+
+  useEffect(() => setToggleStyle(calculateToggleStyle(on, variant)), [on]);
+
+  const handleClick = e => {
+    e.target.checked = !on;
+    setOn(!on);
+    onClick(e);
+  };
+
+  return (
+    <Button {...toggleStyle} {...p} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+};
+
+export default ToggleButton;
