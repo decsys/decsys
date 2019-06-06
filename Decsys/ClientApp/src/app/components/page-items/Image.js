bd984baac8ffb0b17c7f72b99d4206893128e85d
Jonathan Couldridge <jon@jon.gg>
2019-06-06 22:30:09 +0100

    Image width and positioning improvements (#49)
    * Image width and positioning improvements
    
    * version++

--- Decsys/ClientApp/src/app/components/page-items/Image.js
+++ Decsys/ClientApp/src/app/components/page-items/Image.js
@@ -2,9 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import ParamTypes, { buildPropTypes } from "@decsys/param-types";
 import { FlexBox } from "../ui";
+import { Box } from "@smooth-ui/core-sc";
 
 const PageImage = ({ surveyId, id, extension }) => {
-  // TODO: this probably shouldn't be hard coded,
+  // TODO: the path probably shouldn't be hard coded,
   // but it also doesn't need implementation exposing elsewhere
   // this is clean, but we should probably move the path to a config or something
 
@@ -13,24 +14,17 @@ const PageImage = ({ surveyId, id, extension }) => {
 
   // atm we do nothing if the param isn't set
   const src = `/surveys/images/${surveyId}/${id}${extension}?${new Date().getTime()}`;
-  return (
-    (extension && (
-      <FlexBox width={1} justifyContent="center">
+  return extension ? (
+    <FlexBox width={1} justifyContent="center">
+      <Box width="70%" textAlign="center">
         <img
+          style={{ maxWidth: "100%" }}
           alt={`SurveyPageComponent_${surveyId}_${id}`}
           src={src}
-          onError={e => {
-            // TODO: Improve this
-            e.persist();
-            setTimeout(() => {
-              e.target.src = src;
-            }, 100);
-          }}
         />
-      </FlexBox>
-    )) ||
-    null
-  );
+      </Box>
+    </FlexBox>
+  ) : null;
 };
 
 PageImage.params = {
