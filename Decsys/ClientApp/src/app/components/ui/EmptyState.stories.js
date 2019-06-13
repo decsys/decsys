366d09a20dc8f2f891f19d73c12309f9bbe7a003
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-06-13 11:09:46 +0100

    updtes

--- Decsys/ClientApp/src/app/components/ui/EmptyState.stories.js
+++ Decsys/ClientApp/src/app/components/ui/EmptyState.stories.js
@@ -3,8 +3,8 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { text } from "@storybook/addon-knobs";
 import EmptyState from "./EmptyState";
-import { Coffee } from "styled-icons/feather";
-import { PizzaSlice } from "styled-icons/fa-solid";
+import { Coffee } from "styled-icons/feather/Coffee";
+import { PizzaSlice } from "styled-icons/fa-solid/PizzaSlice";
 
 storiesOf("Common UI/EmptyState", module)
   .add("Default", () => <EmptyState message={text("Message", undefined)} />)
