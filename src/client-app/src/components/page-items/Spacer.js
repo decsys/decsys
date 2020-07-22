7e8f102e43c8e22c17273f9cbe6f41f9abc60e7f
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-07-22 10:35:41 +0100

    ⚡chakra Spacer PageItem

--- src/client-app/src/components/page-items/Spacer.js
+++ src/client-app/src/components/page-items/Spacer.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Box } from "@smooth-ui/core-sc";
 import ParamTypes, { buildPropTypes } from "@decsys/param-types";
+import { Box } from "@chakra-ui/core";
 
 const Spacer = ({ height }) => {
   return <Box minHeight={`${height}px`} />;
 };
 
 Spacer.params = {
-  height: ParamTypes.number("Height (px)", 50)
+  height: ParamTypes.number("Height (px)", 50),
 };
 const { pt, defaultProps } = buildPropTypes(Spacer.params);
 Spacer.propTypes = pt;
