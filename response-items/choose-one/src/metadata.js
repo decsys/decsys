064d58a840253fc3ca86abb3548e4b3ec5ade617
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-07-27 11:53:03 +0100

    replaced icon

--- response-items/choose-one/src/metadata.js
+++ response-items/choose-one/src/metadata.js
@@ -1,3 +1,23 @@
-import { List } from "styled-icons/fa-solid/List";
+import React from "react";
 
-export const icon = <List />;
+// https://reactsvgicons.com/ionicons
+// list-sharp
+export const Icon = (p) => (
+  <svg viewBox="0 0 512 512" fill="currentColor" {...p}>
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth={48}
+      d="M144 144h320M144 256h320M144 368h320"
+    />
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+      strokeWidth={32}
+      d="M64 128h32v32H64zM64 240h32v32H64zM64 352h32v32H64z"
+    />
+  </svg>
+);
