b6483f2f2f4ebf6c22642fe1fc4875ccba433139
JosephLavagna <36136861+JosephLavagna@users.noreply.github.com>
2020-07-27 10:55:54 +0100

    Implementation of Repository interfaces (#78)
    * Implemented interfaces for repositories
    
    * changes to proj file
    
    * Additional methods added to the interfaces
    
    * changes to proj file and ParticipantEventRepository
    
    * Changed return of interface methods to DTOs (Models)

---
+++ app/Decsys/Repositories/Contracts/IParticipantEventRepository.cs
@@ -0,0 +1,31 @@
+﻿using Decsys.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decsys.Repositories.Contracts
+{
+    interface IParticipantEventRepository
+    {
+        string GetCollectionName(string participantId, LiteDatabase db);
+        string GetCollectionName(int instanceId, string participantId);
+        string GetParticipantId(int instanceId, string participantId);
+        IEnumerable<ParticipantEvent>_List(int instanceId, string participantId);
+        string GetNextId(string participantId, int instanceId);
+        IEnumerable<ParticipantEvent> List(int instanceId, string participantId);
+        SurveyInstanceResults<ParticipantEvents> Results(int instanceId);
+        void Log(int instanceId, string participantId, ParticipantEvent e);
+
+        ParticipantEvent Last(int instanceId, string participantId, string source, string type);
+        ParticipantEvent Last(int instanceId, string participantId, string type);
+        SurveyInstanceResults<ParticipantResultsSummary> ResultsSummary(int instanceId);
+        List<string> GetAllParticipantLogs(int instanceId);
+
+        ParticipantResultsSummary ResultsSummary(int instanceId, string participantId);
+        ParticipantResultsSummary ParticipantResultsSummary(SurveyInstance instance, string participantId);
+
+    }
+}
---
+++ app/Decsys/Repositories/Contracts/ISurveyRepository.cs
@@ -0,0 +1,24 @@
+﻿using Decsys.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Decsys.Repositories.Contracts
+{
+    public interface ISurveyRepository
+    {
+
+        Survey Get(int id);
+        int Create(string? name = null);
+        int Duplicate(int id);
+        Task<int> Import(Survey survey, List<(string filename, byte[] data)> images);
+        void Delete(int id);
+        void EditName(int id, string name);
+        void Configure(int id, ConfigureSurveyModel config);
+
+
+    }
+}
+ 
