73ccc7abba1fb21c50f64f62f5d2fdc5ddba67e8
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-08-07 15:21:42 +0100

    ✨ endpoint to check app mode

---
+++ app/Decsys/Controllers/ConfigController.cs
@@ -0,0 +1,22 @@
+﻿using Decsys.Config;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Decsys.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class ConfigController : ControllerBase
+    {
+        private readonly AppMode _mode;
+
+        public ConfigController(IOptions<AppMode> mode)
+        {
+            _mode = mode.Value;
+        }
+
+        [HttpGet]
+        public string Index() => _mode.IsWorkshop ? "workshop" : "hosted";
+    }
+}
