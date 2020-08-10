abef7c6e604300a266c82817c3aded4f5053bc0f
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-08-10 14:48:20 +0100

    ✨ Login form works with backend, seeded SuperAdmin

---
+++ app/Decsys/Auth/DataSeeder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Decsys.Auth
+{
+    public static class DataSeeder
+    {
+        public static async Task Seed(
+            UserManager<IdentityUser> users,
+            IPasswordHasher<IdentityUser> passwords,
+            IConfiguration config)
+        {
+            // Seed an initial super user to use for setup
+            if (await users.FindByNameAsync("superadmin@localhost") is null)
+            {
+                // check an actual password has been configured
+                var pwd = config["SuperAdminSeedPassword"];
+                if (string.IsNullOrEmpty(pwd))
+                {
+                    throw new ApplicationException(@"
+A non-empty password must be configured for seeding the inital SuperAdmin User.
+Please set SuperAdminSeedPassword in a user secrets file,
+or the environment variable DOTNET_SuperAdminSeedPassword");
+                }
+
+                var user = new IdentityUser
+                {
+                    UserName = "superadmin@localhost",
+                    Email = "superadmin@localhost",
+                    EmailConfirmed = true
+                };
+
+                user.PasswordHash = passwords.HashPassword(user, pwd);
+
+                await users.CreateAsync(user);
+            }
+        }
+    }
+}
--- app/Decsys/Program.cs
+++ app/Decsys/Program.cs
@@ -1,14 +1,35 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Decsys
 {
     public static class Program
     {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args).Build().Run();
+        public static async Task Main(string[] args)
+        {
+            // Build and Configure the host
+            var host = CreateHostBuilder(args).Build();
+
+            // Any initialisation post configuration but before running the host
+            using (var serviceScope = host.Services?
+                .GetService<IServiceScopeFactory>()?
+                .CreateScope())
+            {
+                if (serviceScope is null)
+                    throw new InvalidOperationException("Service Configuration failure.");
+
+                await Init(serviceScope.ServiceProvider);
+            }
+
+            // Run the host
+            await host.RunAsync();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -20,5 +41,19 @@ namespace Decsys
                     optional: false))
             .ConfigureWebHostDefaults(b =>
                 b.UseStartup<Startup>());
+
+        /// <summary>
+        /// App Initialisation Tasks.
+        /// Runs after `Startup.ConfigureServices()` but before the Host is run.
+        /// </summary>
+        /// <param name="services">The service provider configured by ConfigureServices()</param>
+        public static async Task Init(IServiceProvider services)
+        {
+            // Seed the SuperAdmin user according to configuration
+            await Auth.DataSeeder.Seed(
+                services.GetRequiredService<UserManager<IdentityUser>>(),
+                services.GetRequiredService<IPasswordHasher<IdentityUser>>(),
+                services.GetRequiredService<IConfiguration>());
+        }
     }
 }
