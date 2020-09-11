496b91fd20decc91786c9a31045fa433d322173b
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-09-11 16:41:04 +0100

    ⚡ config changes for superadmin update the existing user

--- app/Decsys/Auth/DataSeeder.cs
+++ app/Decsys/Auth/DataSeeder.cs
@@ -19,26 +19,29 @@ namespace Decsys.Auth
         {
             // Seed an initial super user to use for setup
 
-            
+
+            // prep username
             var configuredUsername = config["Hosted:AdminUsername"];
             var username = string.IsNullOrWhiteSpace(configuredUsername)
                 ? _defaultAdminUsername
                 : configuredUsername;
+            username = $"@{username}"; // Prefix the username to show it's not an email
 
-            // Prefix the username to show it's not an email
-            username = $"@{username}";
-            if (await users.FindByNameAsync(username) is null)
+
+            // check an actual password has been configured
+            var pwd = config["Hosted:AdminPassword"];
+            if (string.IsNullOrEmpty(pwd))
             {
-                // check an actual password has been configured
-                var pwd = config["Hosted:AdminPassword"];
-                if (string.IsNullOrEmpty(pwd))
-                {
-                    throw new ApplicationException(@"
+                throw new ApplicationException(@"
 A non-empty password must be configured for seeding the initial Admin User.
 Please set Hosted:AdminPassword in a settings or user secrets file,
 or the environment variable DOTNET_Hosted_AdminPassword");
-                }
+            }
 
+            // Add the user if they don't exist, else update them,
+            var superAdmin = await users.FindByEmailAsync(_adminEmail);
+            if (superAdmin is null)
+            {
                 var user = new MongoUser
                 {
                     UserName = username,
@@ -51,6 +54,12 @@ or the environment variable DOTNET_Hosted_AdminPassword");
                 await users.CreateAsync(user);
                 await users.AddClaimAsync(user,
                     new Claim(ClaimTypes.Role, "survey.admin"));
+            } else
+            {
+                // update username / password
+                superAdmin.UserName = username;
+                superAdmin.PasswordHash = passwords.HashPassword(superAdmin, pwd);
+                await users.UpdateAsync(superAdmin);
             }
         }
     }
