082b25a4d057a0fd0c48a3bd75e01fa578446243
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-09-17 09:08:24 +0100

    ⚡ AppBar updates to prep for account stuff

--- app/client-app/src/app/layouts/components/DefaultAppBar.js
+++ app/client-app/src/app/layouts/components/DefaultAppBar.js
@@ -1,23 +1,54 @@
 import React from "react";
 import AppBar, { AppBarLink } from "./AppBar";
-import { Icon } from "@chakra-ui/core";
+import {
+  Icon,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+  IconButton,
+  DarkMode,
+  Button,
+} from "@chakra-ui/core";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { BiHelpCircle } from "react-icons/bi";
 import { Link } from "@reach/router";
 
-const DefaultAppBar = ({ brandLink }) => (
-  <AppBar brand="DECSYS" brandLink={brandLink}>
-    <AppBarLink href="/docs">Documentation</AppBarLink>
-    <AppBarLink as={Link} to="/admin">
-      Admin
-    </AppBarLink>
-    <AppBarLink href="http://www.lucidresearch.org/decsys.html" isExternal>
-      About DECSYS{" "}
-      <sup>
-        <Icon as={FaExternalLinkAlt} />
-      </sup>
-    </AppBarLink>
-  </AppBar>
-);
+const DefaultAppBar = ({ brandLink }) => {
+  return (
+    <AppBar brand="DECSYS" brandLink={brandLink}>
+      {true && (
+        <DarkMode>
+          <Button as={Link} variant="ghost" to="/admin">
+            Admin
+          </Button>
+        </DarkMode>
+      )}
+
+      <Menu>
+        <DarkMode>
+          <MenuButton as={IconButton} variant="ghost" icon={<BiHelpCircle />} />
+        </DarkMode>
+        <MenuList>
+          <MenuItem as="a" href="/docs">
+            Documentation
+          </MenuItem>
+          <MenuItem
+            as="a"
+            href="http://www.lucidresearch.org/decsys.html"
+            target="_blank"
+          >
+            <Text>About DECSYS</Text>
+            <sup>
+              <Icon as={FaExternalLinkAlt} />
+            </sup>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </AppBar>
+  );
+};
 DefaultAppBar.defaultProps = { brandLink: "/admin" };
 
 export default DefaultAppBar;
