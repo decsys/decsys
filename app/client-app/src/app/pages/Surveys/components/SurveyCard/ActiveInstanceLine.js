a979ce9c7dbb5b8e138f8da3a21d5fb41ee39f0a
Jonathan Couldridge <jon@jon.gg>
2020-10-12 10:36:09 +0100

    ✨ Respondent count on SurveyCard (#112)
    * 🔥 removing some commented code
    
    * ⚡ typescript yarn sdk for vscode
    
    * ✨ Respondent count on SurveyCard SurveyInfoLine

--- app/client-app/src/app/pages/Surveys/components/SurveyCard/ActiveInstanceLine.js
+++ app/client-app/src/app/pages/Surveys/components/SurveyCard/ActiveInstanceLine.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   Button,
@@ -9,18 +9,33 @@ import {
   Alert,
   AlertIcon,
   Flex,
+  Badge,
 } from "@chakra-ui/core";
 import { Link as RouterLink } from "@reach/router";
 import InstanceValidIdModal from "./InstanceValidIdModal";
 import { InstanceFriendlyIdProvider } from "../../contexts/InstanceFriendlyId";
+import { decode } from "services/instance-id";
+import { getInstanceResultsSummary } from "api/survey-instances";
 
 const ActiveInstanceLine = ({ friendlyId }) => {
   const instanceValidIdModal = useDisclosure();
+  const [results, setResults] = useState({});
+  useEffect(() => {
+    (async () => {
+      const [surveyId, instanceId] = decode(friendlyId);
+      const { data } = await getInstanceResultsSummary(surveyId, instanceId);
+      setResults(data);
+    })();
+  }, [friendlyId]);
 
   return (
     <>
       <Flex align="center" px={2} py={1}>
         <Stack direction="row" alignItems="center">
+          <Badge textAlign="center" colorScheme="cyan" variant="outline" p={1}>
+            {results?.participants?.length ?? 0} respondents
+          </Badge>
+
           <Text fontWeight="bold">Survey ID:</Text>
           <Text>{friendlyId}</Text>
 
@@ -31,7 +46,7 @@ const ActiveInstanceLine = ({ friendlyId }) => {
             /survey/{friendlyId}
           </Link>
 
-          <Stack display={{ base: "none", lg: "inherit" }}>
+          <Stack display={{ base: "none", xl: "inherit" }}>
             <Alert variant="left-accent" status="info" py={1}>
               <AlertIcon />
               Remember to include your DECSYS server's address
