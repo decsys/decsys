e95472c6d5a23e95a9d1f14132a50e47c9f3b28a
Jonathan Couldridge <jcouldridge@outlook.com>
2021-06-01 10:03:18 +0100

    EXternal Lookup entities

---
+++ app/Decsys/Data/Entities/BaseExternalLookup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Decsys.Data.Entities
+{
+    public class BaseExternalLookup
+    {
+        /// <summary>
+        /// DO NOT USE. Only provided for ORM use.
+        /// </summary>
+        [Obsolete]
+        public BaseExternalLookup() { }
+
+        public BaseExternalLookup(string externalKey, string externalId)
+        {
+            ExternalIdKey = externalKey;
+            ExternalIdValue = externalId;
+        }
+
+        /// <summary>
+        /// PK
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The key name to get the external lookup id from
+        /// e.g. for Prolific: `STUDY_ID`
+        /// </summary>
+        public string ExternalIdKey { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The value of the external lookup id for this record
+        /// e.g. for Prolific: `STUDY_ID={ExternalIdValue}`
+        /// </summary>
+        public string ExternalIdValue { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Optionally a key name to use to get the Participant ID from external params
+        /// e.g. for Prolific: `PROLIFIC_PID`
+        /// </summary>
+        public string? ParticipantIdKey { get; set; }
+    }
+}
---
+++ app/Decsys/Data/Entities/Mongo/ExternalLookup.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Decsys.Data.Entities.Mongo
+{
+
+    public class ExternalLookup : BaseExternalLookup
+    {
+        /// <summary>
+        /// DO NOT USE. Only provided for ORM use.
+        /// </summary>
+        [Obsolete]
+        public ExternalLookup() { }
+
+        public ExternalLookup(string externalKey, string externalId, Survey survey)
+            : base(externalKey, externalId)
+        {
+            Survey = survey;
+        }
+
+        public Survey Survey { get; set; } = new();
+
+        public SurveyInstance? Instance { get; set; }
+    }
+}
