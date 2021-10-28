f0dfb36736f1d7c5176e4d4c4cfb9f6e2a366a6b
Jonathan Couldridge <jcouldridge@outlook.com>
2021-10-28 14:20:18 +0100

    DragMarker is always "controlled" now

--- packages/rating-scales/src/vas/DragMarker.stories.js
+++ packages/rating-scales/src/vas/DragMarker.stories.js
@@ -1,4 +1,5 @@
 import { DragMarker } from "./DragMarker";
+import { useState } from "react";
 
 export default {
   title: "VAS+MVAS/DragMarker",
@@ -17,14 +18,51 @@ const BoundMarker = ({ xPos }) => (
   ></div>
 );
 
-export const Basic = () => <DragMarker yAnchor={100} xInit={20} />;
+export const Basic = (args) => {
+  const [markerPos, setMarkerPos] = useState(args.x);
 
-export const WithBounds = () => (
+  args = {
+    ...args,
+    x: markerPos,
+    onDrop: (x) => {
+      setMarkerPos(x);
+      console.log(x);
+    },
+  };
+  console.log(args);
+
+  return (
+    <>
+      <DragMarker {...args} />
+      <input
+        style={{ border: "thin solid grey" }}
+        value={markerPos}
+        onChange={(e) => setMarkerPos(parseInt(e.target.value))}
+      />
+    </>
+  );
+};
+Basic.args = {
+  baseY: 100,
+  x: 100,
+};
+
+export const WithBounds = (args) => (
   <>
     <BoundMarker xPos="200px" />
     <BoundMarker xPos="400px" />
-    <DragMarker yAnchor={100} xMin={200} xMax={400} xInit={300} />
+    <Basic {...args} />
   </>
 );
+WithBounds.args = {
+  baseY: 100,
+  xMin: 200,
+  xMax: 400,
+  x: 300,
+};
 
-export const Labelled = () => <DragMarker label="L" yAnchor={100} xInit={20} />;
+export const Labelled = Basic.bind({});
+Labelled.args = {
+  ...Basic.args,
+  label: "L",
+};
