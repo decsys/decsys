c8bd814a3123cf36981ebb4e0ec6e40c3e2d2d06
Samad <eeysaad@nottingham.ac.uk>
2021-11-17 16:48:36 +0000

    added debounce functionality

--- app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
+++ app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
@@ -1,5 +1,5 @@
 import { Flex, useColorMode, Icon, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import LightHeading from "components/core/LightHeading";
 import { FaGripVertical } from "react-icons/fa";
 import PageActionButtons from "./PageActionButtons";
@@ -8,9 +8,21 @@ import { defaultColorMode } from "themes";
 const PageHeader = ({ page, order, dragHandleProps }) => {
   const { colorMode } = useColorMode();
   const headerStyle = { light: { bg: "gray.300" }, dark: { bg: "gray.600" } };
+
   const [name, setName] = useState(page.name);
-  const handleChange = () =>{// this will handle the change of the name
-  }
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if(firstUpdate.current==true){
+      firstUpdate.current = false
+      return
+    }
+    const delayDebounceFn = setTimeout(() => {
+      console.log("this is when it would send")
+      // Send Axios request here
+    }, 2000)
+    return () => clearTimeout(delayDebounceFn)
+  }, [name])
+ 
   return (
     <Flex
       borderTopRadius={5}
@@ -32,7 +44,7 @@ const PageHeader = ({ page, order, dragHandleProps }) => {
           fontSize="1.3rem"
           value={name}
           ml={2}
-          onChange={handleChange}
+          onChange={(value)=>setName(value.target.value)}
         />
           </Flex>
         
