c6186bfe542d81824883f4701e467e09a80616bf
Samad <eeysaad@nottingham.ac.uk>
2021-11-22 14:23:40 +0000

    added axios request

--- app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
+++ app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
@@ -4,11 +4,12 @@ import LightHeading from "components/core/LightHeading";
 import { FaGripVertical } from "react-icons/fa";
 import PageActionButtons from "./PageActionButtons";
 import { defaultColorMode } from "themes";
-
+import axios from "axios";
+import { useFetchSurvey } from "app/contexts/FetchSurvey";
 const PageHeader = ({ page, order, dragHandleProps }) => {
   const { colorMode } = useColorMode();
   const headerStyle = { light: { bg: "gray.300" }, dark: { bg: "gray.600" } };
-
+  const { id, mutate } = useFetchSurvey();
   const [name, setName] = useState(page.name);
   const firstUpdate = useRef(true);
   useEffect(() => {
@@ -18,7 +19,14 @@ const PageHeader = ({ page, order, dragHandleProps }) => {
     }
     const delayDebounceFn = setTimeout(() => {
       console.log("this is when it would send")
+      console.log(page)
+      console.log(id)
       // Send Axios request here
+      axios.put(
+        `/api/survey/${id}/pages/${page.id}/name`,
+        {name:"new name"}//,
+        //withHeaders(await authorization_BearerToken())
+      ).then(res=>console.log(res))
     }, 2000)
     return () => clearTimeout(delayDebounceFn)
   }, [name])
