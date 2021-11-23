b5ddad11ac50c686d6b9bb03b91f2e3430d6e585
Jonathan Couldridge <jon@jon.gg>
2021-11-23 13:38:55 +0000

    🐛 ParamsEditor number params no longer save as strings (#159)
    * fix the params editor stringifying number params
    
    * rendering page items also coerces and validates their param types
    
    * clearer comments
    
    * i can't type
    
    * version++

--- app/client-app/src/components/shared/PageItemRender.js
+++ app/client-app/src/components/shared/PageItemRender.js
@@ -1,28 +1,44 @@
 import { createElement } from "react";
+import { ensureParamTypes } from "@decsys/param-types";
+import { ErrorBoundary } from "components/ErrorBoundary";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 
-const PageItemRender = ({ _context, component, params }) => {
+const PageItemRenderError = ({ __BoundaryError }) => (
+  <Alert status="error">
+    <AlertIcon />
+    {__BoundaryError.message}
+  </Alert>
+);
+
+const PageItemRenderer = ({ _context, component, params }) => {
   if (!component) return null;
 
   // TODO: document the props signature change here.
   // Components now receive ({_context, ...params, ...results}) props
   // _context is prepared in the call site; this allows Previews to behave differently to Surveys
 
-  // merge default Params and set ones
   // TODO: change defaults behaviour.
   // Empty params reverting to defaults is annoying UX and sometimes prevents intentionally empty params
-  const paramDefaults = Object.keys(component.params).reduce((agg, x) => {
-    agg[x] = component.params[x].defaultValue;
-    return agg;
-  }, {});
+  // to be fair, such a change is probably more in the editor/backend than here when rendering;
+  // the behaviour in ensureParamTypes is probably correct
+
+  // Apply defaults for unset params, and coerce types and validate any set params
+  const paramValues = ensureParamTypes(component.params, params);
 
   // TODO: manage passing results here too, when the platform does that again
   const results = {};
 
   return createElement(component, {
-    ...{ ...paramDefaults, ...params },
+    ...{ ...params, ...paramValues }, // we also need to keep any custom editor param values (not specified in paramTypes)
     ...results,
     _context,
   });
 };
+
+const PageItemRender = (p) => (
+  <ErrorBoundary resetOnPropChanges fallback={<PageItemRenderError />}>
+    <PageItemRenderer {...p} />
+  </ErrorBoundary>
+);
 
 export default PageItemRender;
