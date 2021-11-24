26d61c058b4751f74e6c1981c53dacf248f0a43e
Samad <eeysaad@nottingham.ac.uk>
2021-11-24 11:26:03 +0000

    moved axios call to api file

--- app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
+++ app/client-app/src/app/pages/Editor/components/PageList/PageHeader.js
@@ -1,29 +1,23 @@
 import { Flex, useColorMode, Icon, Input } from "@chakra-ui/react";
-import { useState, useEffect, useRef } from "react";
 import LightHeading from "components/core/LightHeading";
 import { FaGripVertical } from "react-icons/fa";
 import PageActionButtons from "./PageActionButtons";
 import { defaultColorMode } from "themes";
-import axios from "axios";
+import { setSurveyPageName } from "api/pages";
 import { useFetchSurvey } from "app/contexts/FetchSurvey";
-import { authorization_BearerToken, withHeaders, contentType_AppJson } from "api/helpers";
 import { useDerivedState } from "hooks/useDerivedState";
 import useDeferredAction from "hooks/useDeferredAction";
 const PageHeader = ({ page, order, dragHandleProps }) => {
   const { colorMode } = useColorMode();
   const headerStyle = { light: { bg: "gray.300" }, dark: { bg: "gray.600" } };
   const { id, mutate } = useFetchSurvey();
-  //const [name, setName] = useState(page.name);
+  
   const [name, setName] = useDerivedState(page.name);
   //const { saveName } = useSurveyCardActions();
   // just need to make a save name function in an api file
   const handleNameSave = async (value) =>{
     //saveName(id, value, setNameState);
-    axios.put(
-      `/api/surveys/${id}/pages/${page.id}/name`,
-      JSON.stringify(value),
-      withHeaders(contentType_AppJson, await authorization_BearerToken())
-    )
+    setSurveyPageName(id,page.id,value)
   }
   const deferredSave = useDeferredAction(handleNameSave);
   const handleChange = ({ target: { value } }) => {
