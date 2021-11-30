26025b2e36af90c3311dc585be6d1de900b1b853
Jonathan Couldridge <jcouldridge@outlook.com>
2021-11-30 14:24:35 +0000

    backend BuiltIn Page Items lookup

---
+++ app/Decsys/Constants/BuiltInPageItems.cs
@@ -0,0 +1,22 @@
+﻿namespace Decsys.Constants;
+
+public readonly record struct PageItemMetadata(string Type, string questionContent);
+
+public static class BuiltInPageItems
+{
+    private static readonly Dictionary<string, PageItemMetadata> pageItems = new()
+    {
+        ["heading"] = new("heading", "text"),
+        ["paragraph"] = new("paragraph", "text"),
+        ["image"] = new("image", "questionContent"),
+        ["spacer"] = new("spacer", "questionContent"),
+    };
+
+    /// <summary>
+    /// Determine if the specified Page Item type is a built-in type the backend is aware of
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public static bool IsBuiltIn(string type) => pageItems.ContainsKey(type);
+}
+
