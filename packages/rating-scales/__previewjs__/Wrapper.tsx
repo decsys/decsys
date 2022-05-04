6f376d0c900e99bfb04f0cf8bed5e0249b4e64cc
Jon Couldridge <jcouldridge@outlook.com>
2022-05-04 16:00:14 +0100

    resetting pnpm to just docs for rebuilding repo with vite

--- packages/rating-scales/__previewjs__/Wrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { ChakraProvider } from "@chakra-ui/react";
-
-export const Wrapper: React.FC = ({ children }) => (
-  <ChakraProvider>
-    <div className="wrapped">{children}</div>
-  </ChakraProvider>
-);
