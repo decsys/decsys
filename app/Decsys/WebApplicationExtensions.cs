a5e803f51692c530db58029d6849400539ecc2da
Jon Couldridge <jcouldridge@outlook.com>
2023-03-20 16:54:36 +0000

    Migrating Azure Pipelines CI to Github Actions (#187)
    * templating the web-frontend build closer to the old azure pipelines
    
    * hopefully fixing composite action manifests
    
    * workflows for response item ci builds
    
    * fixing a param typo
    
    * pnpm install rating-scales deps for scale reponses
    
    * simpler app versioning, with optional git hash
    
    * wip publish workflow
    
    * added pr trigger for publish testing
    
    * syntax fixes
    
    * syntax fixes
    
    * syntax fixes
    
    * syntax fixes
    
    * default fallback on matrix args didn't work
    
    * js dependency fixes
    
    * wip dotnet publish
    
    * syntax fixes
    
    * dotnet publish includes js artifacts
    
    * removed old azure pipelines
    
    * fixing some warnings
    
    * docs website ci/cd

--- app/Decsys/WebApplicationExtensions.cs
+++ app/Decsys/WebApplicationExtensions.cs
@@ -11,21 +11,6 @@ namespace Decsys
 {
     public static class WebApplicationExtensions
     {
-        public static void UseAppVersion(this WebApplication app)
-        {
-            var version = app.Services.GetRequiredService<VersionInformationService>();
-
-            // start with the keys from the file, cast back to the original dictionary
-            var versionInfo = (Dictionary<string, string>)
-                (version.ByKey("file", "version.txt")
-                    ?? new Dictionary<string, string>());
-
-            // Add further keys, so everything's at the top level
-            versionInfo["API App"] = (string)version.EntryAssembly();
-
-            app.UseVersion(versionInfo);
-        }
-
         public static void UseAppStaticFiles(this WebApplication app, AppMode mode)
         {
             // in future we may want to make this a configurable list.
