7e88b4dd330f50de523f57a0384b5d55ec73f464
Moiz <moiziqbal5021@gmail.com>
2023-04-03 11:43:30 +0100

    added vite storybook

---
+++ response-items/.storybook/main.js
@@ -0,0 +1,17 @@
+// .storybook/main.js|ts
+
+export default {
+  stories: ["../src/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-mdx-gfm",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
