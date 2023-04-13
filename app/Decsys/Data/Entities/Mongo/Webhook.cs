d1b56213ccfd7264658d072c5c52ae133241a191
Andy Rae <hello@rae.li>
2023-04-13 12:02:04 +0100

    Add entity

--- app/Decsys/Data/Entities/Mongo/Webhook.cs
+++ app/Decsys/Data/Entities/Mongo/Webhook.cs
@@ -1,15 +1,34 @@
+using MongoDB.Bson;
+
 namespace Decsys.Data.Entities.Mongo;
 
 public class Webhook
 {
-    public int SurveyId { get; set; }
+    public ObjectId SurveyId { get; set; }
     public string CallbackUrl { get; set; } = string.Empty;
     public string Secret { get; set; } = string.Empty;
     public bool VerifySsl { get; set; }
-    public string EventTypeTrigger { get; set; } = string.Empty;
-
-    // public ? TriggerParameters;
-    // public ? TriggerFilters;
-
-
+    public EventType EventType { get; set; }
+    public Dictionary<string, string> TriggerParameters { get; set; } = new Dictionary<string, string>();
+    public List<TriggerFilter> TriggerFilters { get; set; } = new List<TriggerFilter>();
 }
+
+public class TriggerFilter
+{
+    public string Name { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+    public FilterOperator Operator { get; set; }
+}
+
+public enum FilterOperator
+{
+    EQUALS,
+    NOT_EQUALS,
+}
+
+public enum EventType
+{
+    PAGE_NAVIGATION,
+    SURVEY_COMPLETE
+}
+
