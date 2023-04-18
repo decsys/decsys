34e0412c89cb6e5dbac96a912cb9a48015aa9340
Andy Rae <hello@rae.li>
2023-04-18 12:07:03 +0100

    Progress

--- app/Decsys/Data/Entities/Mongo/Webhook.cs
+++ app/Decsys/Data/Entities/Mongo/Webhook.cs
@@ -1,10 +1,8 @@
-using MongoDB.Bson;
-
 namespace Decsys.Data.Entities.Mongo;
 
 public class Webhook
 {
-    public ObjectId SurveyId { get; set; }
+    public int SurveyId { get; set; }
     public string CallbackUrl { get; set; } = string.Empty;
     public string Secret { get; set; } = string.Empty;
     public bool VerifySsl { get; set; }
