9a86c473e6e2c698201a61c67b0f84ad1158a5fb
Andy Rae <hello@rae.li>
2023-04-21 15:43:06 +0100

    Add non-implemented litedb repository

---
+++ app/Decsys/Repositories/LiteDb/LiteDbWebhookRepository.cs
@@ -0,0 +1,12 @@
+using Decsys.Models.Webhooks;
+using Decsys.Repositories.Contracts;
+
+namespace Decsys.Repositories.LiteDb;
+
+public class LiteDbWebhookRepository : IWebhookRepository
+{
+    public WebhookModel Create(WebhookModel webhook)
+    {
+        throw new NotImplementedException();
+    }
+}
