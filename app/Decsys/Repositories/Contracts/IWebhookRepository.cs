6ef4c596a31951c9c3dc67099d248b270b4b4dcb
Andy Rae <hello@rae.li>
2023-05-02 15:33:43 +0100

    Add docstring

--- app/Decsys/Repositories/Contracts/IWebhookRepository.cs
+++ app/Decsys/Repositories/Contracts/IWebhookRepository.cs
@@ -7,7 +7,7 @@ public interface IWebhookRepository
     /// <summary>
     /// Create a new webhook.
     /// </summary>
-    /// <param name="webhook"></param>
+    /// <param name="webhook">The webhook model to create.</param>
     /// <returns>The created Webhook Id</returns>
     int Create(WebhookModel webhook);
 }
