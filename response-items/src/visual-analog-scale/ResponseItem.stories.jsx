41770c433bb1fa86656314c48e57d7a6c3086357
Moiz <moiziqbal5021@gmail.com>
2023-05-15 13:34:32 +0100

    storybook defaults for vas

--- response-items/src/visual-analog-scale/ResponseItem.stories.jsx
+++ response-items/src/visual-analog-scale/ResponseItem.stories.jsx
@@ -1,6 +1,49 @@
 import ResponseItem from "./ResponseItem";
 import Icon from "./Icon";
 
+export default {
+  title: "Visual Analog Scale",
+  component: ResponseItem,
+  argTypes: {
+    useConfidenceInput: {
+      options: ["None", "input", "scale"],
+      control: { type: "radio" },
+    },
+  },
+};
+
+const props = {
+  barLeftMargin: 10,
+  barTopMargin: 50,
+  barRightMargin: 10,
+  barThickness: 8,
+  barMaxValue: 100,
+  barMinValue: 0,
+};
+
+const dummyResults = [
+  {
+    value: 60,
+    confidence: 50,
+  },
+  {
+    confidence: 50,
+    value: 0,
+  },
+  {
+    confidence: 35,
+    value: 50,
+  },
+  {
+    confidence: 55,
+    value: 40,
+  },
+  {
+    confidence: 90,
+    value: 80,
+  },
+];
+
 const _context = {
   setIsValidResponse: (isValid) => {
     console.log("set valid response", isValid);
@@ -13,45 +56,36 @@ const _context = {
   },
 };
 
-export default {
-  title: "Visual Analog Scale",
-  component: ResponseItem,
-  argTypes: {
-    behaviour: {
-      options: ["Speirs-Bridge 2010", "Hesketh, Pryor & Hesketh 1988"],
-      control: { type: "radio" },
-    },
+const stats = (stats) => () =>
+  (
+    <div>
+      {Object.keys(stats.stats).map((x) => (
+        <div key={x}>
+          <h4>{x}</h4>
+          <p>{stats.stats[x]}</p>
+        </div>
+      ))}
+    </div>
+  );
 
-    useConfidenceInput: {
-      options: ["None", "input", "scale"],
-      control: { type: "radio" },
-    },
-    labelAlignment: {
-      options: ["above", "below"],
-      control: { type: "radio" },
-    },
-    buttons: {
-      options: ["None", "Undo", "Reset", "Both"],
-      control: { type: "radio" },
-    },
-  },
-};
-
-export const primary = {
+export const Basic = {
   render: (args) => {
     return <ResponseItem {...args} _context={_context} />;
   },
   args: {
-    barLeftMargin: 10,
-    barTopMargin: 50,
-    barColor: "black",
-    barThickness: 8,
-    barRightMargin: 10,
-    barMaxValue: 100,
-    barMinValue: 0,
-    labelColor: "black",
-    scaleMarkerColor: "black",
+    ...props,
   },
 };
+
+export const NumericStats = stats(
+  ResponseItem.stats({ ...ResponseItem.args, ...props }, dummyResults)
+);
+
+export const NumericStatsMissingConfidence = stats(
+  ResponseItem.stats(
+    { ...ResponseItem.args, ...props },
+    dummyResults.map((x) => ({ ...x, confidence: undefined }))
+  )
+);
 
 export const MetadataIcon = () => <Icon width="24px" />;
