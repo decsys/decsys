3567d28eb9cff6f4d95a485616c5ac0da0e5e8d5
Moiz <moiziqbal5021@gmail.com>
2023-05-26 16:31:56 +0100

    wordlist controller

--- app/Decsys/Controllers/WordlistsController.cs
+++ app/Decsys/Controllers/WordlistsController.cs
@@ -1,10 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Decsys.Auth;
+using Decsys.Data.Entities;
+using Decsys.Models.Wordlist;
+using Decsys.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Security.Claims;
 
-namespace Decsys.Controllers;
-public class WordlistsController : Controller
+namespace Decsys.Controllers
 {
-    public IActionResult Index()
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordlistsController : ControllerBase
     {
-        return View();
+        private readonly WordlistService _service;
+        private readonly IMapper _mapper;
+
+        public WordlistsController(WordlistService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+
+        [HttpPost]
+        [Authorize(Policy = nameof(AuthPolicies.IsSurveyAdmin))]
+        [SwaggerOperation("Get or create a wordlist for the current user")]
+        [SwaggerResponse(200, "Wordlist created or recieved.")]
+        [SwaggerResponse(401, "User is not authenticated")]
+        [SwaggerResponse(403, "User is not authorized to perform this operation")]
+        public ActionResult<UserWordlist> GetOrCreate()
+        {
+            var userId = "userID";
+
+            var wordlist = _service.List(userId).FirstOrDefault();
+
+            if (wordlist == null)
+            {
+                var newId = _service.Create(userId);
+                wordlist = _service.List(userId).FirstOrDefault(w => w.Id == newId);
+            }
+
+            var model = _mapper.Map<UserWordlist>(wordlist);
+
+            return Ok(model);
+        }
     }
 }
+
