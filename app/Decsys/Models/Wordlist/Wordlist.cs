5c35d7ff01178eb582f48dabdf93584bc53ce9f3
Moiz <moiziqbal5021@gmail.com>
2023-05-30 17:45:06 +0100

    fixed typo

--- app/Decsys/Models/Wordlist/Wordlist.cs
+++ app/Decsys/Models/Wordlist/Wordlist.cs
@@ -3,7 +3,7 @@ namespace Decsys.Models.Wordlist;
 public class Wordlist
 {
     public int Id { get; set; }
-    public WordListOptions Options { get; set; } = new();
+    public WordlistOptions Options { get; set; } = new();
     public List<WordlistRules> Rules { get; set; } = new();
     public List<WordlistWord> ExcludeWords { get; set; } = new();
     public List<WordlistWord> IncludeWords { get; set; } = new();
