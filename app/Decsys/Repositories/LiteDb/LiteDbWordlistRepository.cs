8665c28dc05efb792845690f26cb5be355bf74d8
Moiz <moiziqbal5021@gmail.com>
2023-06-01 15:04:58 +0100

    added comment

--- app/Decsys/Repositories/LiteDb/LiteDbWordlistRepository.cs
+++ app/Decsys/Repositories/LiteDb/LiteDbWordlistRepository.cs
@@ -10,9 +10,10 @@ namespace Decsys.Repositories.LiteDb;
 
 public class LiteDbWordlistRepository : IWordlistRepository
 {
+    //Wordlist will not work for LiteDb as wordlist requires a user(ownerId), liteDb does not have users set up
     public UserWordlist List(string ownerId) 
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(); 
     }
 
     public Task<UserWordlist> Create(string ownerId)
