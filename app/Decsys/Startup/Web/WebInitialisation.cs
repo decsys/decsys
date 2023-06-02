c3d5bb8a1c8a67db003c0306c90f445a0114f042
Andy Rae <hello@rae.li>
2023-06-02 09:40:03 +0100

    Update comment

--- app/Decsys/Startup/Web/WebInitialisation.cs
+++ app/Decsys/Startup/Web/WebInitialisation.cs
@@ -8,10 +8,10 @@ namespace Decsys.Startup.Web;
 public static class WebInitialisation
 {
     /// <summary>
-    /// 
+    /// Initialisation before the app runs.
     /// </summary>
-    /// <param name="app"></param>
-    /// <param name="mode"></param>
+    /// <param name="app">The web application to initialise.</param>
+    /// <param name="mode">The mode of the app to initialise.</param>
     /// <exception cref="InvalidOperationException"></exception>
     public static async Task Initialise(this WebApplication app, AppMode mode)
     {
