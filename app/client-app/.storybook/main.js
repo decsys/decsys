58a52322cfbe9e66fbdd3b85fb311eb86b305d1b
Moiz <moiziqbal5021@gmail.com>
2023-06-08 16:14:41 +0100

    storybook changes and files renamed

--- app/client-app/.storybook/main.js
+++ app/client-app/.storybook/main.js
@@ -1,15 +1,17 @@
-export default {
-  stories: ["../src/**/*.stories.jsx"],
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-mdx-gfm",
+    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: true,
+    autodocs: "tag",
   },
 };
+export default config;
