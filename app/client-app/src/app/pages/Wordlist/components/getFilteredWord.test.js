e61d5201509bf3767a5b899c2eab8344fddd7748
Moiz <moiziqbal5021@gmail.com>
2023-06-14 16:04:23 +0100

    is excluded defaults to false and added a new test

--- app/client-app/src/app/pages/Wordlist/components/getFilteredWord.test.js
+++ app/client-app/src/app/pages/Wordlist/components/getFilteredWord.test.js
@@ -1,9 +1,29 @@
-import * as sut from "./helpers.js";
+import { getFilteredWordList } from "./helpers.js";
 import { describe, expect, it } from "vitest";
 
-describe("return an object", () => {
-  it("should get an empty list"),
-    () => {
-      expect(sut.getFilteredWordList()).toBe({});
-    };
+describe("getFilteredWordList", () => {
+  it("should return an empty list when no words are passed", () => {
+    const words = [];
+    const excludeWordsDict = {};
+    const wordsType = "noun";
+
+    expect(getFilteredWordList(words, excludeWordsDict, wordsType)).toEqual([]);
+  });
+
+  it("should map a list of words to WordCard models, even if no dictionary or type is provided", () => {
+    const words = ["apple", "banana", "cherry"];
+    const excludeWordsDict = {};
+    const wordType = "noun";
+
+    const result = getFilteredWordList(words, excludeWordsDict, wordType);
+
+    const expected = words.map((word) => ({
+      type: wordType,
+      word,
+      isCustomWord: false,
+      isExcluded: false,
+    }));
+
+    expect(result).toEqual(expected);
+  });
 });
--- app/client-app/src/app/pages/Wordlist/components/helpers.js
+++ app/client-app/src/app/pages/Wordlist/components/helpers.js
@@ -1,19 +1,25 @@
 /**
- * Generates a filtered list of WordCard models based on the source word list,
+ * Generates a list of WordCard models based on the source word list,
  * excluded words dictionary, and word type.
  * @param {string[]} words - The source word list.
- * @param {Object} excludeWordsDict - The dictionary of excluded words.
+ * @param {Object} excludeWordsDict - The dictionary of words to check for exclusion.
  * @param {string} wordsType - The type of the words (e.g., 'adjective', 'noun').
- * @returns {Object[]} - The filtered list of WordCard models.
+ * @returns {Object[]} - The list of WordCard models.
  */
-
-export const getFilteredWordList = (words, excludeWordsDict, wordsType) => {
+export const getFilteredWordList = (
+  words = [],
+  excludeWordsDict = {},
+  wordsType
+) => {
   return words.map((word) => {
+    const excludedWord = excludeWordsDict[word];
+    const isExcluded = excludedWord && excludedWord.type === wordsType;
+
     return {
       type: wordsType,
       word,
       isCustomWord: false,
-      isExcluded: !!excludeWordsDict[word],
+      isExcluded: isExcluded || false,
     };
   });
 };
