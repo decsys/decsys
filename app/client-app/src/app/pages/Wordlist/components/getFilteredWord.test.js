98f27a0597eb9a7e0a0c09a632b1f04f285de60f
Moiz <moiziqbal5021@gmail.com>
2023-06-15 12:23:47 +0100

    explicit expected array for test

--- app/client-app/src/app/pages/Wordlist/components/getFilteredWord.test.js
+++ app/client-app/src/app/pages/Wordlist/components/getFilteredWord.test.js
@@ -4,25 +4,20 @@ import { describe, expect, it } from "vitest";
 describe("getFilteredWordList", () => {
   it("should return an empty list when no words are passed", () => {
     const words = [];
-    const excludeWordsDict = {};
-    const wordsType = "noun";
+    const excludeWordsDict = { rabbit: { type: "noun" } };
 
-    expect(getFilteredWordList(words, excludeWordsDict, wordsType)).toEqual([]);
+    expect(getFilteredWordList(words, excludeWordsDict, "noun")).toEqual([]);
   });
 
   it("should map a list of words to WordCard models, even if no dictionary or type is provided", () => {
-    const words = ["apple", "banana", "cherry"];
+    const words = ["rabbit", "cat", "dog"];
     const excludeWordsDict = {};
-    const wordType = "noun";
-
-    const result = getFilteredWordList(words, excludeWordsDict, wordType);
-
-    const expected = words.map((word) => ({
-      type: wordType,
-      word,
-      isCustomWord: false,
-      isExcluded: false,
-    }));
+    const result = getFilteredWordList(words, excludeWordsDict, "noun");
+    const expected = [
+      { type: "noun", word: "rabbit", isCustomWord: false, isExcluded: false },
+      { type: "noun", word: "cat", isCustomWord: false, isExcluded: false },
+      { type: "noun", word: "dog", isCustomWord: false, isExcluded: false },
+    ];
 
     expect(result).toEqual(expected);
   });
@@ -30,21 +25,92 @@ describe("getFilteredWordList", () => {
   it("should map a list of words to WordCard models and correctly exclude words", () => {
     const words = ["apple", "banana", "cherry"];
     const excludeWordsDict = {
-      banana: { type: "noun" },
-      cherry: { type: "noun" },
+      cat: { type: "noun" },
+      dog: { type: "noun" },
     };
-    const wordsType = "adjective";
+    const result = getFilteredWordList(words, excludeWordsDict, "adjective");
+    const expected = [
+      {
+        type: "adjective",
+        word: "apple",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+      {
+        type: "adjective",
+        word: "banana",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+      {
+        type: "adjective",
+        word: "cherry",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+    ];
 
-    const result = getFilteredWordList(words, excludeWordsDict, wordsType);
+    expect(result).toEqual(expected);
+  });
 
-    const expected = words.map((word) => ({
-      type: wordsType,
-      word,
-      isCustomWord: false,
-      isExcluded:
-        !!excludeWordsDict[word] && excludeWordsDict[word].type === wordsType,
-    }));
+  it("should return a complete list mapped to WordCard models when a dictionary of excluded words and a different type argument are provided", () => {
+    const words = ["apple", "banana", "cherry"];
+    const excludeWordsDict = {
+      cat: { type: "noun" },
+      dog: { type: "noun" },
+    };
+    const result = getFilteredWordList(words, excludeWordsDict, "adjective");
+    const expected = [
+      {
+        type: "adjective",
+        word: "apple",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+      {
+        type: "adjective",
+        word: "banana",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+      {
+        type: "adjective",
+        word: "cherry",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+    ];
 
+    expect(result).toEqual(expected);
+  });
+
+  it("should return a modified list, containing only the words not matched by the excluded words of the matching type", () => {
+    const words = ["cat", "dog", "rabbit"];
+    const excludeWordsDict = {
+      cat: { type: "noun" },
+      dog: { type: "noun" },
+    };
+    const result = getFilteredWordList(words, excludeWordsDict, "noun");
+    const expected = [
+      {
+        type: "noun",
+        word: "cat",
+        isCustomWord: false,
+        isExcluded: true,
+      },
+      {
+        type: "noun",
+        word: "dog",
+        isCustomWord: false,
+        isExcluded: true,
+      },
+      {
+        type: "noun",
+        word: "rabbit",
+        isCustomWord: false,
+        isExcluded: false,
+      },
+    ];
     expect(result).toEqual(expected);
   });
 });
