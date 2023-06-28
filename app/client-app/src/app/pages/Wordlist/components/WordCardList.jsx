f8ea6616cf333b8e35efbc69f98cda5ee90d5509
Moiz <moiziqbal5021@gmail.com>
2023-06-28 08:58:16 +0100

    remove console.log

--- app/client-app/src/app/pages/Wordlist/components/WordCardList.jsx
+++ app/client-app/src/app/pages/Wordlist/components/WordCardList.jsx
@@ -8,23 +8,17 @@ export const WordCardList = ({
   onSort,
   toggleExclude,
   outputList,
-}) => {
-  console.log(sorting);
-  return (
-    <Stack mt={2}>
-      <WordlistSortingAndFilteringPanel
-        data={cards}
-        sorting={sorting}
-        onSort={onSort}
-      />
-      <Stack
-        borderColor="gray.200"
-        boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;"
-      >
-        {outputList.map((card, index) => (
-          <WordCard key={index} {...card} onToggleExclude={toggleExclude} />
-        ))}
-      </Stack>
+}) => (
+  <Stack mt={2}>
+    <WordlistSortingAndFilteringPanel
+      data={cards}
+      sorting={sorting}
+      onSort={onSort}
+    />
+    <Stack borderColor="gray.200" boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;">
+      {outputList.map((card, index) => (
+        <WordCard key={index} {...card} onToggleExclude={toggleExclude} />
+      ))}
     </Stack>
-  );
-};
+  </Stack>
+);
