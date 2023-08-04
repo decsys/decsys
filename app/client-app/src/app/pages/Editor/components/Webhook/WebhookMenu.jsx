7f5525a3df9e58acbb7003b31fa2777d4b5401ff
Moiz <moiziqbal5021@gmail.com>
2023-08-04 10:22:02 +0100

    refactor

--- app/client-app/src/app/pages/Editor/components/Webhook/WebhookMenu.jsx
+++ app/client-app/src/app/pages/Editor/components/Webhook/WebhookMenu.jsx
@@ -1,53 +1,21 @@
-import {
-  Button,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  IconButton,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
-  Badge,
-  Heading,
-  Text,
-  Box,
-  HStack,
-} from "@chakra-ui/react";
-import { FaEllipsisV, FaPlus } from "react-icons/fa";
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
 import { useFetchSurvey } from "app/contexts/FetchSurvey";
-import { createWebhook, useWebhook } from "api/webhooks";
 import WebhookForm from "./WebhookForm";
-import { ActionCard } from "components/shared/ActionCard";
-import { FaTrash, FaFilter } from "react-icons/fa";
+import WebhooksModal from "./WebhookModal";
+import { useWebhook } from "api/webhooks";
+import { useDisclosure } from "@chakra-ui/react";
+import { createWebhook } from "api/webhooks";
 
 const WebhookMenu = () => {
-  const [badgeProperties, setBadgeProperties] = useState([]);
-
   const { id: surveyId } = useFetchSurvey();
   const { data, mutate } = useWebhook(surveyId);
-
-  console.log(data);
-
-  const {
-    isOpen: isWebhooksModalOpen,
-    onOpen: openWebhooksModal,
-    onClose: closeWebhooksModal,
-  } = useDisclosure();
+  const finalRef = useRef(null);
 
   const {
     isOpen: isFormOpen,
     onOpen: onFormOpen,
     onClose: onFormClose,
   } = useDisclosure();
-  const finalRef = useRef(null);
 
   const handleAddWebhook = () => {
     onFormOpen();
@@ -77,99 +45,11 @@ const WebhookMenu = () => {
 
   return (
     <>
-      <Menu>
-        <MenuButton
-          border="thin solid"
-          borderColor="gray.500"
-          as={IconButton}
-          icon={<FaEllipsisV />}
-          boxSize={"40px"}
-        />
-        <MenuList>
-          <MenuItem onClick={openWebhooksModal}>Manage Webhooks ...</MenuItem>
-        </MenuList>
-      </Menu>
-
-      <Modal
-        finalFocusRef={finalRef}
-        isOpen={isWebhooksModalOpen}
-        onClose={closeWebhooksModal}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Webhooks</ModalHeader>
-          <ModalCloseButton />
-
-          <ModalBody>
-            {data &&
-              data.map((webhook) => (
-                <Box p={2} key={webhook.callbackUrl}>
-                  <ActionCard
-                    title={
-                      <Flex justify="space-between" align="center">
-                        <Heading as="h4" size="md">
-                          {webhook.callbackUrl}
-                        </Heading>
-                        <IconButton
-                          colorScheme="red"
-                          size="sm"
-                          icon={<FaTrash />}
-                        />
-                      </Flex>
-                    }
-                  >
-                    <Text>
-                      {!webhook.triggerCriteria.hasCustomTriggers && (
-                        <Badge colorScheme="green" p={1}>
-                          <Text>Trigger on every event</Text>
-                        </Badge>
-                      )}
-                      {webhook.triggerCriteria.eventTypes.PAGE_NAVIGATION !==
-                        null &&
-                        webhook.triggerCriteria.eventTypes.PAGE_NAVIGATION
-                          .length === 0 &&
-                        webhook.triggerCriteria.hasCustomTriggers && (
-                          <Badge colorScheme="blue" p={1}>
-                            <HStack spacing={2}>
-                              <Text>Page Navigation</Text>
-                            </HStack>
-                          </Badge>
-                        )}
-                      {webhook.triggerCriteria.eventTypes.PAGE_NAVIGATION !==
-                        null &&
-                        webhook.triggerCriteria.eventTypes.PAGE_NAVIGATION
-                          .length > 0 &&
-                        webhook.triggerCriteria.hasCustomTriggers && (
-                          <Badge colorScheme="blue" p={1}>
-                            <HStack spacing={2}>
-                              <Text>Page Navigation with filters</Text>
-                              <FaFilter />
-                            </HStack>
-                          </Badge>
-                        )}
-                    </Text>
-                  </ActionCard>
-                </Box>
-              ))}
-          </ModalBody>
-
-          <Flex align="start" direction="column" pl={6}>
-            <Button
-              colorScheme="green"
-              size="sm"
-              leftIcon={<FaPlus />}
-              onClick={handleAddWebhook}
-            >
-              Create a Webhook
-            </Button>
-          </Flex>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={closeWebhooksModal}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <WebhooksModal
+        finalRef={finalRef}
+        webhooks={data}
+        onAddWebhook={handleAddWebhook}
+      />
       <WebhookForm
         isOpen={isFormOpen}
         onClose={onFormClose}
---
+++ app/client-app/src/app/pages/Editor/components/Webhook/WebhookModal.jsx
@@ -0,0 +1,84 @@
+import {
+  Button,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Box,
+} from "@chakra-ui/react";
+import { FaEllipsisV, FaPlus } from "react-icons/fa";
+import WebhookActionCard from "./WebhookActionCard";
+
+const WebhooksModal = ({ finalRef, webhooks, onAddWebhook }) => {
+  const {
+    isOpen: isWebhooksModalOpen,
+    onOpen: openWebhooksModal,
+    onClose: closeWebhooksModal,
+  } = useDisclosure();
+
+  return (
+    <>
+      <Menu>
+        <MenuButton
+          border="thin solid"
+          borderColor="gray.500"
+          as={IconButton}
+          icon={<FaEllipsisV />}
+          boxSize={"40px"}
+        />
+        <MenuList>
+          <MenuItem onClick={openWebhooksModal}>Manage Webhooks ...</MenuItem>
+        </MenuList>
+      </Menu>
+
+      <Modal
+        finalFocusRef={finalRef}
+        isOpen={isWebhooksModalOpen}
+        onClose={closeWebhooksModal}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Webhooks</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            {webhooks &&
+              webhooks.map((webhook) => (
+                <Box p={2} key={webhook.callbackUrl}>
+                  <WebhookActionCard webhook={webhook} />
+                </Box>
+              ))}
+          </ModalBody>
+
+          <Flex align="start" direction="column" pl={6}>
+            <Button
+              colorScheme="green"
+              size="sm"
+              leftIcon={<FaPlus />}
+              onClick={onAddWebhook}
+            >
+              Create a Webhook
+            </Button>
+          </Flex>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={closeWebhooksModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default WebhooksModal;
