09ec615719ac1ccc2131028c54e3acb974783dfd
Moiz <moiziqbal5021@gmail.com>
2023-08-08 10:25:41 +0100

    ViewWebhook model

---
+++ app/Decsys/Models/Webhooks/ViewWebhook.cs
@@ -0,0 +1,15 @@
+namespace Decsys.Models.Webhooks;
+
+public class ViewWebhook
+{
+    
+    public int SurveyId { get; set; }
+    public string CallbackUrl { get; set; } = string.Empty;
+    public bool HasSecret { get; set; }
+    public bool VerifySsl { get; set; }
+    /// <summary>
+    /// The trigger criteria to test against.
+    /// </summary>
+    public TriggerCriteria TriggerCriteria { get; set; } = new();
+
+}
