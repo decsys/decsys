3604111533cdfec2f76e30aacf412c9ea66fffdf
Moiz <moiziqbal5021@gmail.com>
2023-11-15 12:43:47 +0000

    delete old stories and update question story

--- packages/rating-scales/src/core/Question.stories.tsx
+++ packages/rating-scales/src/core/Question.stories.tsx
@@ -1,24 +1,57 @@
-import React from "react";
-import { Question, QuestionProps } from "./Question";
+import { Question } from "./Question";
 
 export default {
-  title: "core/Question",
+  title: "Example/Question",
   component: Question,
-  decorators: [
-    (Story) => (
-      <div
-        style={{
-          height: "300px",
-          width: "100%",
-        }}
-      >
-        <Story />
-      </div>
-    ),
-  ],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    textColor: { control: "color" },
+    fontFamily: { control: "text" },
+    fontSize: { control: "text" },
+    xAlign: {
+      control: "select",
+      options: ["left", "center", "right"],
+    },
+  },
 };
 
-export const Basic = (args: QuestionProps) => <Question {...args} />;
-Basic.args = {
-  children: "Hello there!",
+export const Default = {
+  args: {
+    textColor: "black",
+    topMargin: "10%",
+    xMargin: "5%",
+    fontFamily: "Arial",
+    fontSize: "1.6em",
+    xAlign: "left",
+    children: "Your question here",
+  },
+};
+
+export const CenterAligned = {
+  args: {
+    ...Default.args,
+    xAlign: "center",
+  },
+};
+
+export const RightAligned = {
+  args: {
+    ...Default.args,
+    xAlign: "right",
+  },
+};
+
+export const CustomStyle = {
+  args: {
+    textColor: "blue",
+    topMargin: "20%",
+    xMargin: "10%",
+    fontFamily: "Times New Roman",
+    fontSize: "2em",
+    xAlign: "center",
+    children: "Your custom styled question",
+  },
 };
