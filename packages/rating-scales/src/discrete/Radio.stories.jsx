faf8e74f3b37fba40eb10714864ae87fb97fe4fc
Moiz <moiziqbal5021@gmail.com>
2023-11-16 15:10:26 +0000

    radio button story fixes

--- packages/rating-scales/src/discrete/Radio.stories.jsx
+++ packages/rating-scales/src/discrete/Radio.stories.jsx
@@ -16,7 +16,6 @@ export default {
     value: { control: "text" },
     id: { control: "text" },
     labelColor: { control: "color" },
-    barColor: { control: "color" },
     fontFamily: { control: "text" },
     defaultChecked: { control: "boolean" },
     fontSize: { control: "text" },
@@ -25,9 +24,15 @@ export default {
   },
 };
 
-export const Basic = (args) => <Radio {...args} />;
-Basic.args = {
-  index: 0,
-  value: "1",
-  id: "radio",
+export const Basic = {
+  args: {
+    index: "0",
+    value: "1",
+    id: "radio-button",
+    labelColor: "black",
+    fontFamily: "Ariel",
+    defaultChecked: Boolean(false),
+    labelAbove: Boolean(false),
+    secondaryLabel: "First",
+  },
 };
