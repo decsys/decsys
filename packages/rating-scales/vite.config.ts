d32b9ac9af828625bedb73985def5b818ad62513
Moiz <moiziqbal5021@gmail.com>
2023-11-16 10:46:18 +0000

    vite emotion package added

---
+++ packages/rating-scales/vite.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
+});
