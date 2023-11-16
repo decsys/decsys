91757000a44a379e4b9598aa805e48052d933e5e
Moiz <moiziqbal5021@gmail.com>
2023-11-16 12:40:55 +0000

    scale bar stories

--- packages/rating-scales/src/core/ScaleBar.stories.jsx
+++ packages/rating-scales/src/core/ScaleBar.stories.jsx
@@ -1,8 +1,21 @@
-import { FlexContainer, ScaleBar } from "./ScaleBar";
+import { useCallback, useState } from "react";
+import { ScaleBar, FlexContainer } from "./ScaleBar";
 
 export default {
   title: "core/ScaleBar",
   component: ScaleBar,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          height: "300px",
+          width: "100%",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
   tags: ["autodocs"],
   argTypes: {
     leftMargin: { control: "text" },
@@ -13,12 +26,41 @@ export default {
   },
 };
 
-export const Default = {
-  args: {
-    leftMargin: "10%",
-    rightMargin: "10%",
-    topMargin: "50%",
-    thickness: "0.2em",
-    barColor: "black",
-  },
+// Story Definitions
+
+export const Basic = (args) => <ScaleBar {...args} />;
+
+export const WithChildren = (args) => (
+  <ScaleBar {...args}>
+    <FlexContainer>
+      {[1, 2, 3].map((n) => (
+        <div style={{ marginTop: `${n}0px` }} key={n}>
+          {n}
+        </div>
+      ))}
+    </FlexContainer>
+  </ScaleBar>
+);
+
+export const WithRef = (args) => {
+  const [width, setWidth] = useState();
+  const barRef = useCallback((bar) => {
+    const resize = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        setWidth(entry.target.getBoundingClientRect().width);
+      }
+    });
+
+    resize.observe(bar);
+  }, []);
+
+  return (
+    <>
+      <div>Bar width: {width} (Try resizing the window)</div>
+      <ScaleBar {...args} ref={barRef} />
+    </>
+  );
+};
+WithRef.args = {
+  topMargin: "80px",
 };
