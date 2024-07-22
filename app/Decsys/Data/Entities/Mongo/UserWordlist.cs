1c68b8d6354ac9362c56985fefaae445a86e1f65
Moiz Iqbal <moiziqbal5021@gmail.com>
2024-07-22 15:08:52 +0100

    list wordists backend

--- app/Decsys/Data/Entities/Mongo/UserWordlist.cs
+++ app/Decsys/Data/Entities/Mongo/UserWordlist.cs
@@ -6,6 +6,7 @@ public class UserWordlist
 {
     public ObjectId Id { get; set; }
     public string Owner { get; set; } = string.Empty;
+    public string Name { get; set; } = "Untitled Wordlist";
     public WordlistOptions Options { get; set; } = new();
     public List<WordlistRules> Rules { get; set; } = new();
     public List<WordlistWord> ExcludedBuiltins { get; set; } = new();
