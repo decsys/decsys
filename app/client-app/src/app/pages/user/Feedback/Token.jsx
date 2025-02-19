4b524037c5e22986f6d476afde9a3fee60a6f0a9
Moiz Iqbal <moiziqbal5021@gmail.com>
2025-02-19 11:48:03 +0000

    unused import

--- app/client-app/src/app/pages/user/Feedback/Token.jsx
+++ app/client-app/src/app/pages/user/Feedback/Token.jsx
@@ -1,6 +1,5 @@
 import { useQueryStringViewModel } from "hooks/useQueryString";
 import TryThisAlert from "components/core/TryThisAlert";
-import { Utf8ToBase64Url } from "services/data-structures";
 
 export const TokenExpired = () => {
   const { errors, userId } = useQueryStringViewModel();
